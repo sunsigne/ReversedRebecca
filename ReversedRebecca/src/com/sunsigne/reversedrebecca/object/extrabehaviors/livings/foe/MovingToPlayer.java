2c4215818720f8cbf275c182c960ef0015ecbed8
Florian Murcia <florian.murcia@hotmail.fr>
2021-11-25 12:22:27 +0100

    creation of the foe behavios "moving to player"

---
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/object/extrabehaviors/livings/foe/MovingToPlayer.java
@@ -0,0 +1,68 @@
+package com.sunsigne.reversedrebecca.object.extrabehaviors.livings.foe;
+
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+
+import com.sunsigne.reversedrebecca.object.extrabehaviors.Behavior;
+import com.sunsigne.reversedrebecca.object.extrabehaviors.livings.player.Player;
+import com.sunsigne.reversedrebecca.pattern.PlayerFinder;
+
+public class MovingToPlayer implements Behavior {
+
+	public MovingToPlayer(Foe foe) {
+		this.foe = foe;
+	}
+
+	////////// BEHAVIOR ////////////
+
+	private Foe foe;
+
+	@Override
+	public Foe getExtraBehaviorsObject() {
+		return foe;
+	}
+
+	////////// TICK ////////////
+
+	private Player player;
+	
+	@Override
+	public void tick() {
+		if(player == null) initPlayer();
+		else FollowPlayer();
+	}
+	
+	private void initPlayer() {
+		player = new PlayerFinder().getPlayer();
+	}
+	
+	private void FollowPlayer() {
+		float diffX = foe.getX() - player.getX();
+		float diffY = foe.getY() - player.getY();
+		float distance = (float) Math.sqrt(Math.pow(diffX, 2) + Math.pow(diffY, 2));
+
+		foe.setVelX(foe.speed * Math.round((-1 / distance) * diffX));
+		foe.setVelY(foe.speed * Math.round((-1 / distance) * diffY));
+	}
+
+	////////// RENDER ////////////
+
+
+	@Override
+	public void render(Graphics g) {
+
+	}
+
+	////////// KEYBOARD ////////////
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+
+	}
+
+}
