85fd8ffe0caeda384f27aa24064dbd71e4d2d571
Florian Murcia <florian.murcia@hotmail.fr>
2021-11-25 09:57:29 +0100

    creation of a PlayerFinder

---
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/pattern/PlayerFinder.java
@@ -0,0 +1,22 @@
+package com.sunsigne.reversedrebecca.pattern;
+
+import com.sunsigne.reversedrebecca.object.Player;
+import com.sunsigne.reversedrebecca.system.mainloop.Updatable;
+import com.sunsigne.reversedrebecca.world.LAYER;
+
+public class PlayerFinder {
+
+	public Player getPlayer() {
+				
+	for (LAYER tempLayer : LAYER.values()) {
+		var list = tempLayer.getHandler().getList();
+					
+		for (Updatable tempUpdatable : list) {
+			if (tempUpdatable instanceof Player)
+				return (Player) tempUpdatable;
+			}
+		}
+	return null;
+	}
+	
+}
