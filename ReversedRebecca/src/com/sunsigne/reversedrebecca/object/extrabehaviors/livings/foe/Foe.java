d7d8d7180845c8982410a4f006e7879747cb9506
Florian Murcia <florian.murcia@hotmail.fr>
2021-11-25 12:35:38 +0100

    Foe now push the player when contact

--- ReversedRebecca/src/com/sunsigne/reversedrebecca/object/extrabehaviors/livings/foe/Foe.java
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/object/extrabehaviors/livings/foe/Foe.java
@@ -3,29 +3,41 @@ package com.sunsigne.reversedrebecca.object.extrabehaviors.livings.foe;
 import com.sunsigne.reversedrebecca.object.characteristics.CollisionDetector;
 import com.sunsigne.reversedrebecca.object.extrabehaviors.Behavior;
 import com.sunsigne.reversedrebecca.object.extrabehaviors.livings.LivingObject;
+import com.sunsigne.reversedrebecca.object.extrabehaviors.livings.MoveWhenPushed;
+import com.sunsigne.reversedrebecca.object.extrabehaviors.livings.player.Player;
 
 public class Foe extends LivingObject {
 
 	public int speed = 6;
-	
+
 	public Foe(String name, int x, int y) {
-		super(name ,x, y);
+		super(name, x, y);
 		addFoeBehaviors();
 	}
 
 	////////// BEHAVIOR ////////////
 
 	public Behavior movingToPlayer = new MovingToPlayer(this);
-		
+
 	private void addFoeBehaviors() {
 		addBehavior(movingToPlayer);
 	}
 
 	////////// COLLISION ////////////
-	
+
 	@Override
 	public void collidingReaction(CollisionDetector detectorObject) {
-		blockPass(detectorObject);
+		if (detectorObject instanceof Player)
+			pushPlayer(detectorObject);
+		else
+			blockPass(detectorObject);
+	}
+
+	private void pushPlayer(CollisionDetector detectorObject) {
+		Player player = (Player) detectorObject;
+		// stun();
+		var moveWhenPushed = (MoveWhenPushed) player.moveWhenPushed;
+		moveWhenPushed.pushToward(getFacing());
 	}
 
 }
