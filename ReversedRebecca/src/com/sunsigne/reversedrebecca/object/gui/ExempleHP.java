9716b9d65dd5af82b60f83529fa1a16306ee131c
Florian Murcia <florian.murcia@hotmail.fr>
2022-01-15 16:41:25 +0100

    removal of a test feature

--- ReversedRebecca/src/com/sunsigne/reversedrebecca/object/gui/ExempleHP.java
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/object/gui/ExempleHP.java
@@ -3,15 +3,12 @@ package com.sunsigne.reversedrebecca.object.gui;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 
 import com.sunsigne.reversedrebecca.object.GameObject;
 import com.sunsigne.reversedrebecca.ressources.images.Texture;
-import com.sunsigne.reversedrebecca.system.controllers.mouse.MouseUserEvent;
-import com.sunsigne.reversedrebecca.system.controllers.mouse.MouseController;
 
-public class ExempleHP extends GameObject implements Texture, MouseUserEvent {
+public class ExempleHP extends GameObject implements Texture {
 
 	public ExempleHP() {
 		super(0, 0);
@@ -46,28 +43,4 @@ public class ExempleHP extends GameObject implements Texture, MouseUserEvent {
 		g.drawString(text, getX() + 20, getY() + 60);
 	}
 	
-	//////////RENDER ////////////
-	
-	private MouseController mouseController = new MouseController(this);
-
-	@Override
-	public MouseController getMouseController() {
-		return  mouseController;
-	}
-
-	@Override
-	public void mousePressed(MouseEvent e) {
-		int mx = e.getX();
-		int my = e.getY();
-		
-		if(mouseOver(mx, my, getRect()))
-		{
-			System.out.println("hello world");
-		}
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent e) {
-	}
-
 }
