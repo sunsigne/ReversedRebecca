5b53bf868c093b55f627c30bb31b36673c8e8171
Florian Murcia <florian.murcia@hotmail.fr>
2022-01-16 16:06:03 +0100

    ButtonObject now asks for main behavior when created

--- ReversedRebecca/src/com/sunsigne/reversedrebecca/object/extrabehaviors/menu/ButtonObject.java
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/object/extrabehaviors/menu/ButtonObject.java
@@ -1,29 +1,50 @@
 package com.sunsigne.reversedrebecca.object.extrabehaviors.menu;
 
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 
 import com.sunsigne.reversedrebecca.object.extrabehaviors.ExtraBehaviorsObject;
-import com.sunsigne.reversedrebecca.object.extrabehaviors.behaviors.Behavior;
-import com.sunsigne.reversedrebecca.system.controllers.mouse.MouseController;
+import com.sunsigne.reversedrebecca.pattern.GenericListener;
 
 public class ButtonObject extends ExtraBehaviorsObject {
 
-	public ButtonObject(int x, int y, int w, int h) {
+	public ButtonObject(int x, int y, int w, int h, GenericListener onPress, GenericListener onRelease) {
 		super(x, y, w, h);
+		
+		this.onPress = onPress;
+		this.onRelease = onRelease;
 	}
 
 	////////// BEHAVIOR ////////////
 
+	private GenericListener onPress;
+	private GenericListener onRelease;
+	
+	////////// RENDER ////////////
+	
+	@Override
+	public void render(Graphics g) {
+		g.setColor(Color.RED);
+		g.fillRect(getX(), getY(), getWidth(), getHeight());
+	}
+	
+	////////// MOUSE ////////////
+	
 	@Override
 	public void mousePressed(MouseEvent e) {
 		if (mouseOver(e, getRect())) {
-			System.out.println("lel");
+			if(onPress != null)
+				onPress.doAction();
 		}
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
-
+		if (mouseOver(e, getRect())) {
+			if(onPress != null)
+				onRelease.doAction();
+		}
 	}	
 
 }
