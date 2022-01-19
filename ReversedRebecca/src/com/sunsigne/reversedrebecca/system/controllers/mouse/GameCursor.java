d5108b36ef5ed00973b5433ac1c26f84c736ae00
Florian Murcia <florian.murcia@hotmail.fr>
2022-01-19 19:40:49 +0100

    Removal of Update from GameCursor (MouseEvent already does that)

--- ReversedRebecca/src/com/sunsigne/reversedrebecca/system/controllers/mouse/GameCursor.java
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/system/controllers/mouse/GameCursor.java
@@ -1,46 +1,30 @@
 package com.sunsigne.reversedrebecca.system.controllers.mouse;
 
 import java.awt.Cursor;
-import java.awt.Graphics;
-import java.awt.MouseInfo;
 import java.awt.Point;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 
 import com.sunsigne.reversedrebecca.system.mainloop.Game;
-import com.sunsigne.reversedrebecca.system.mainloop.Updatable;
 
-public class GameCursor implements Updatable {
+public class GameCursor {
 
-	////////// VISIBILITY ////////////
-	
 	public void setVisible(boolean visible) {
-		BufferedImage emptyImg = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
-		Cursor emptyCursor = Toolkit.getDefaultToolkit().createCustomCursor(emptyImg, new Point(0, 0), "empty cursor");
-		Cursor cursor = visible ? Cursor.getDefaultCursor() : emptyCursor;
+		
+		Cursor cursor = getEmptyCursor();
+
+		if (visible)
+			cursor = Cursor.getDefaultCursor();
+
 		Game.getInstance().setCursor(cursor);
 	}
-	
-	////////// POSITION ////////////
 
-	private static Point pos;
+	private Cursor getEmptyCursor() {
 
-	public Point getPos() {
-		return pos;
-	}
-		
-	////////// TICK ////////////
+		BufferedImage emptyImg = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
+		Cursor cursor = Toolkit.getDefaultToolkit().createCustomCursor(emptyImg, new Point(0, 0), "empty cursor");
 
-	@Override
-	public void tick() {
-		pos = MouseInfo.getPointerInfo().getLocation();
-	}
-
-	////////// RENDER ////////////
-	
-	@Override
-	public void render(Graphics g) {
-
+		return cursor;
 	}
 
 }
