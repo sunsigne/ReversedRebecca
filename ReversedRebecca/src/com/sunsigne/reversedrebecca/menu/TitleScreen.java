c9ccc8038f7437b36b8ea0991d0ff38891771c6f
Florian Murcia <florian.murcia@hotmail.fr>
2022-01-25 15:28:37 +0100

    TitleScreen is a little bit less chaotic

--- ReversedRebecca/src/com/sunsigne/reversedrebecca/menu/TitleScreen.java
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/menu/TitleScreen.java
@@ -2,15 +2,12 @@ package com.sunsigne.reversedrebecca.menu;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.event.KeyEvent;
 
 import com.sunsigne.reversedrebecca.object.extrabehaviors.buttons.ButtonObject;
 import com.sunsigne.reversedrebecca.pattern.GenericListener;
 import com.sunsigne.reversedrebecca.ressources.layers.LAYER;
 import com.sunsigne.reversedrebecca.system.OldConductor;
 import com.sunsigne.reversedrebecca.system.Window;
-import com.sunsigne.reversedrebecca.system.controllers.keyboard.KeyboardController;
-import com.sunsigne.reversedrebecca.system.controllers.keyboard.KeyboardEvent;
 import com.sunsigne.reversedrebecca.system.mainloop.Updatable;
 import com.sunsigne.reversedrebecca.world.World;
 
@@ -19,28 +16,33 @@ public class TitleScreen implements Updatable {
 	public TitleScreen() {
 		LAYER.MENU.addObject(this);
 		
-		GenericListener onPress = () -> {};
-		GenericListener onRelease = () -> new OldConductor().stopApp();
-		
-		quitButton = new ButtonObject("quit", 500, 500, 500, 100, onPress, onRelease);
-		LAYER.MENU.addObject(quitButton);
-		
-		onPress = () -> {};
-		onRelease = () -> 
-		{
-			LAYER.MENU.getHandler().clear();
-			new World("lvl001");
-		};
-		
-		quitButton = new ButtonObject("start", 500, 300, 500, 100, onPress, onRelease);
+		createPlayButton();
+		createQuitButton();
+	}
+
+	////////// BUTTONS ////////////
+
+	private void createPlayButton() {
+		GenericListener onPress = () -> startWorld();
+		GenericListener onRelease = null;
+
+		ButtonObject playButton = new ButtonObject("start", 500, 300, 500, 90, onPress, onRelease);
+		LAYER.MENU.addObject(playButton);
+	}	
+	
+	private void startWorld() {
+		LAYER.MENU.getHandler().clear();
+		new World("lvl001");
+	}
+
+	private void createQuitButton() {
+		GenericListener onPress = () -> new OldConductor().stopApp();
+		GenericListener onRelease = null;
+
+		ButtonObject quitButton = new ButtonObject("quit", 500, 500, 500, 90, onPress, onRelease);
 		LAYER.MENU.addObject(quitButton);
 	}
-	
-	////////// BUTTONS ////////////
-	
-	private ButtonObject playerButton;
-	private ButtonObject quitButton;
-	
+
 	////////// TICK ////////////
 
 	@Override
