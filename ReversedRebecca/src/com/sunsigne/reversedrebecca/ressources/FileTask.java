726dd7b03aa7c988b9020fdd1e75b227f4c88e91
Florian Murcia <florian.murcia@hotmail.fr>
2022-01-26 02:40:00 +0100

    creation of the class FileTask (to read and write csv files)

---
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/ressources/FileTask.java
@@ -0,0 +1,104 @@
+package com.sunsigne.reversedrebecca.ressources;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
+
+public class FileTask {
+
+	public boolean doesExist(String path) {
+		File file = new File("ressources/" + path);
+		return file.exists();
+	}
+
+	public String read(String path) {
+
+		return read(path, 0);
+	}
+
+	public String read(String path, int lineToRead) {
+
+		File file = new File("ressources/" + path);
+		Scanner scan = null;
+		String content = "";
+		int count = 0;
+
+		try {
+			if (file.exists()) {
+				scan = new Scanner(file, "UTF-8");
+				boolean flag = false;
+
+				// read the whole file
+				if (lineToRead == 0) {
+					while (scan.hasNextLine()) {
+						if (!flag) {
+							content = content.concat(scan.nextLine());
+							flag = true;
+						} else
+							content = content.concat(String.format("%n" + scan.nextLine()));
+					}
+				}
+
+				// read one specific line
+				else {
+					while (count != lineToRead) {
+						if (lineToRead < 0)
+							break;
+						content = scan.nextLine();
+						count++;
+					}
+				}
+
+				scan.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return content;
+	}
+
+	public void write(String path, String text) {
+		write(path, text, 0);
+	}
+
+	public void write(String path, String text, int lineToReplace) {
+		File file = new File("ressources/" + path);
+		String fileContent = read(path);
+		String[] alllines = fileContent.split(System.getProperty("line.separator"));
+		int size = alllines.length;
+
+		FileWriter writer = null;
+
+		try {
+			writer = new FileWriter(file);
+
+			// write the whole file
+			if (lineToReplace == 0) {
+				writer.write(text);
+			}
+
+			// write one specific line
+			else {
+				for (int i = 0; i < size; i++) {
+					if (i != lineToReplace)
+						writer.write(String.format(alllines[i] + "%n"));
+					else
+						writer.write(String.format(text + "%n"));
+				}
+			}
+
+			writer.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void delete(String path) {
+		File file = new File("ressources/" + path);
+		if (file.exists())
+			file.delete();
+	}
+
+}
