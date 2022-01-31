55cf62f0e65df7a72e165e177c6f4b8173168fd9
Florian Murcia <florian.murcia@hotmail.fr>
2022-01-31 08:55:31 +0100

    addition of the method drawOutlinesString

--- ReversedRebecca/src/com/sunsigne/reversedrebecca/pattern/render/TextDecoration.java
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/pattern/render/TextDecoration.java
@@ -22,8 +22,30 @@ public class TextDecoration {
 
 		int gap = 4;
 		int[] shadow_rect = { rect[0] + gap, rect[1] + gap, rect[2], rect[3] };
-		
+
 		new TextDecoration().drawCenteredString(g, text, font, shadow_color, shadow_rect);
+		new TextDecoration().drawCenteredString(g, text, font, text_color, rect);
+	}
+
+	public void drawOutlinesString(Graphics g, String text, Font font, int[] rect) {
+
+		drawOutlinesString(g, text, font, Color.WHITE, Color.BLACK, rect);
+	}
+
+	public void drawOutlinesString(Graphics g, String text, Font font, Color text_color, Color outlines_color,
+			int[] rect) {
+
+		int gap = 4;
+		int[] up_left_rect = { rect[0] - gap, rect[1] - gap, rect[2], rect[3] };
+		int[] up_right_rect = { rect[0] + gap, rect[1] - gap, rect[2], rect[3] };
+		int[] down_left_rect = { rect[0] - gap, rect[1] + gap, rect[2], rect[3] };
+		int[] down_right_rect = { rect[0] + gap, rect[1] + gap, rect[2], rect[3] };
+
+		new TextDecoration().drawCenteredString(g, text, font, outlines_color, up_left_rect);
+		new TextDecoration().drawCenteredString(g, text, font, outlines_color, up_right_rect);
+		new TextDecoration().drawCenteredString(g, text, font, outlines_color, down_left_rect);
+		new TextDecoration().drawCenteredString(g, text, font, outlines_color, down_right_rect);
+
 		new TextDecoration().drawCenteredString(g, text, font, text_color, rect);
 	}
 
