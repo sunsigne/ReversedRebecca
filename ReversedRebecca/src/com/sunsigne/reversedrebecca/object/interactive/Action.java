86dac372a37ebc78bee667d4ca9722da4b262bca
Florian Murcia <florian.murcia@hotmail.fr>
2022-02-03 15:45:29 +0100

    removal of the useless ToolAction

--- ReversedRebecca/src/com/sunsigne/reversedrebecca/object/interactive/Action.java
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/object/interactive/Action.java
@@ -1,12 +1,21 @@
 package com.sunsigne.reversedrebecca.object.interactive;
 
+import com.sunsigne.reversedrebecca.characteristics.ToolPlayer;
+import com.sunsigne.reversedrebecca.object.characteristics.Difficulty;
 import com.sunsigne.reversedrebecca.pattern.GenericListener;
 
 public class Action {
 
-	public Action(InteractiveControlObject interactiveControlObject, String name, GenericListener listener, int keyEvent) {
+	public Action(InteractiveControlObject interactiveControlObject, String name, GenericListener listener,
+			int keyEvent) {
+		this(interactiveControlObject, name, null, listener, keyEvent);
+	}
+
+	public Action(InteractiveControlObject interactiveControlObject, String name, ToolPlayer toolPlayer,
+			GenericListener listener, int keyEvent) {
 		this.interactiveControlObject = interactiveControlObject;
 		this.name = name;
+		this.toolPlayer = toolPlayer;
 		this.keyEvent = keyEvent;
 		this.listener = listener;
 	}
@@ -27,13 +36,28 @@ public class Action {
 
 	////////// INTERACTION ////////////
 
-	protected GenericListener listener;
+	private ToolPlayer toolPlayer;
+	private GenericListener listener;
 
 	public void doAction() {
 		if (listener == null)
 			return;
 
-		listener.doAction();
+		if (toolPlayer == null) {
+			listener.doAction();
+			return;
+		}
+
+		if (getInteractiveControlObject() instanceof Difficulty == false) {
+			System.err.println(
+					"the InteractiveControlObject need to implements Difficulty if a ToolPlayer stands in the constructor of an Action");
+			return;
+		}
+
+		Difficulty difficultyObject = (Difficulty) getInteractiveControlObject();
+
+		if (difficultyObject.getDifficulty() == toolPlayer.getDifficulty())
+			listener.doAction();
 	}
 
 	////////// KEYBOARD ////////////
--- ReversedRebecca/src/com/sunsigne/reversedrebecca/object/interactive/puzzler/UnlockAction.java
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/object/interactive/puzzler/UnlockAction.java
@@ -3,23 +3,24 @@ package com.sunsigne.reversedrebecca.object.interactive.puzzler;
 import java.awt.event.KeyEvent;
 
 import com.sunsigne.reversedrebecca.characteristics.KeyToolPlayer;
+import com.sunsigne.reversedrebecca.object.interactive.Action;
 import com.sunsigne.reversedrebecca.object.interactive.InteractiveControlObject;
-import com.sunsigne.reversedrebecca.object.interactive.ToolAction;
 import com.sunsigne.reversedrebecca.pattern.GenericListener;
 import com.sunsigne.reversedrebecca.ressources.lang.Translatable;
 
-public class UnlockAction extends ToolAction {
+public class UnlockAction extends Action {
 
 	public UnlockAction(InteractiveControlObject interactiveControlObject) {
-		super(interactiveControlObject, new Translatable().getTranslatedText("UNLOCK", "actions/door.csv", 2), new KeyToolPlayer(), unlockAction(interactiveControlObject),
-				KeyEvent.VK_E);
+		super(interactiveControlObject, new Translatable().getTranslatedText("UNLOCK", "actions/door.csv", 2),
+				new KeyToolPlayer(), unlockAction(interactiveControlObject), KeyEvent.VK_E);
 	}
 
 	////////// INTERACTION ////////////
 
 	private static GenericListener unlockAction(InteractiveControlObject interactiveControlObject) {
 		GenericListener listener = () -> {
-			NullDoorObject nullDoor = new NullDoorObject(interactiveControlObject.getX(), interactiveControlObject.getY());
+			NullDoorObject nullDoor = new NullDoorObject(interactiveControlObject.getX(),
+					interactiveControlObject.getY());
 			interactiveControlObject.getHandler().addObject(nullDoor);
 			interactiveControlObject.getHandler().removeObject(interactiveControlObject);
 		};
