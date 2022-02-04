1c1b25ba9ce71761e129654dfcd3e073c41dd761
Florian Murcia <florian.murcia@hotmail.fr>
2022-02-04 08:54:16 +0100

    addition of a new layer : WORLD_TEXT

--- ReversedRebecca/src/com/sunsigne/reversedrebecca/physic/laws/UpdateLayersLaw.java
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/physic/laws/UpdateLayersLaw.java
@@ -10,58 +10,59 @@ import com.sunsigne.reversedrebecca.world.World;
 public class UpdateLayersLaw implements PhysicLaw {
 
 	////////// PHYSIC LAW ////////////
-	
+
 	public UpdateLayersLaw() {
 		PhysicList.getList().addObject(this);
 	}
-	
+
 	private static PhysicLaw physicLaw = new UpdateLayersLaw();
-	
+
 	@Override
 	public PhysicLaw getPhysicLaw() {
 		return physicLaw;
 	}
-	
+
 	////////// TICK ////////////
 
 	private LAYER layer;
-	
+
 	@Override
 	public void tick(Updatable object) {
-		if(World.get() == null)
-		{
+		if (World.get() == null) {
 			layer = LAYER.DEBUG;
 			return;
 		}
-		
-		if(object instanceof World == false)
+
+		if (object instanceof World == false)
 			return;
-		
+
 		World world = (World) object;
-		
+
 		if (layer == world.getLayer(false))
 			return;
-		
+
 		updateLayerVisibilities(world);
 		registerNewLayer(world);
 	}
-	
+
 	public void updateLayerVisibilities(World world) {
 		boolean hideRendering = false;
 
-		for(LAYER tempLayer : LAYER.values())
-		{
-			if(!tempLayer.getHandler().isCameraDependant())
+		for (LAYER tempLayer : LAYER.values()) {
+			if (tempLayer == LAYER.WORLD_TEXT)
+				continue;
+
+			if (!tempLayer.getHandler().isCameraDependant())
 				continue;
 
 			tempLayer.getHandler().setHideRendering(hideRendering);
-			
-			if(world.getLayer(true) == tempLayer)
+
+			if (world.getLayer(true) == tempLayer)
 				hideRendering = true;
-		}			
+		}
 	}
-		
-	private void registerNewLayer(World world) {		
+
+	private void registerNewLayer(World world) {
 		layer.getHandler().removeObject(world);
 		layer = world.getLayer(false);
 		layer.addObject(world);
