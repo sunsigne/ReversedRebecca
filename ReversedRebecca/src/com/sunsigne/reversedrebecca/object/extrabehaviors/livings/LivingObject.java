3c4f2b7749e24a6938804582662483536bcf2fc3
Florian Murcia <florian.murcia@hotmail.fr>
2022-02-05 12:52:16 +0100

    removal of the useless implementation in LivingObject

--- ReversedRebecca/src/com/sunsigne/reversedrebecca/object/extrabehaviors/livings/LivingObject.java
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/object/extrabehaviors/livings/LivingObject.java
@@ -1,14 +1,13 @@
 package com.sunsigne.reversedrebecca.object.extrabehaviors.livings;
 
 import com.sunsigne.reversedrebecca.object.characteristics.CollisionDetector;
-import com.sunsigne.reversedrebecca.object.characteristics.Facing;
 import com.sunsigne.reversedrebecca.object.extrabehaviors.ExtraBehaviorsObject;
 import com.sunsigne.reversedrebecca.object.extrabehaviors.behaviors.Behavior;
 import com.sunsigne.reversedrebecca.object.extrabehaviors.livings.behaviors.MoveWhenPushed;
 import com.sunsigne.reversedrebecca.object.extrabehaviors.livings.behaviors.WalkingRender;
 import com.sunsigne.reversedrebecca.object.extrabehaviors.livings.behaviors.WatchingDirection;
 
-public abstract class LivingObject extends ExtraBehaviorsObject implements Facing, CollisionDetector {
+public abstract class LivingObject extends ExtraBehaviorsObject implements CollisionDetector {
 
 	public LivingObject(String name, int x, int y) {
 		super(x, y);
