c8067a596891c4dc8003bbb21d5fac3874edf15a
Florian Murcia <florian.murcia@hotmail.fr>
2022-02-06 13:13:51 +0100

    creation of the MultiToolMode (debug mode)

---
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/physic/debug/MultiToolMode.java
@@ -0,0 +1,62 @@
+package com.sunsigne.reversedrebecca.physic.debug;
+
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+
+import com.sunsigne.reversedrebecca.characteristics.tools.ToolPlayerList;
+import com.sunsigne.reversedrebecca.object.characteristics.Difficulty.LVL;
+import com.sunsigne.reversedrebecca.physic.laws.PhysicLaw;
+import com.sunsigne.reversedrebecca.system.mainloop.Updatable;
+
+public class MultiToolMode extends DebugMode {
+
+	////////// PHYSIC LAW ////////////
+
+	private static PhysicLaw physicLaw = new MultiToolMode();
+
+	@Override
+	public PhysicLaw getPhysicLaw() {
+		return physicLaw;
+	}
+
+	////////// NAME ////////////
+
+	@Override
+	public String getName() {
+		return "debug_multitool_mode";
+	}
+
+	////////// TICK ////////////
+
+	@Override
+	public void tick(Updatable object) {
+
+	}
+
+	////////// RENDER ////////////
+
+	@Override
+	public void beforeObjectRender(Graphics g, Updatable object) {
+
+	}
+
+	@Override
+	public void afterObjectRender(Graphics g, Updatable object) {
+
+	}
+
+	////////// KEYBOARD ////////////
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		int key = e.getKeyCode();
+		if (key == getKeyEvent()) {
+			cycle();
+			if (getState())
+				ToolPlayerList.getList().getList().forEach(tempTool -> tempTool.setDifficulty(LVL.RED));
+			else
+				ToolPlayerList.getList().getList().forEach(tempTool -> tempTool.setDifficulty(LVL.NULL));
+		}
+	}
+
+}
