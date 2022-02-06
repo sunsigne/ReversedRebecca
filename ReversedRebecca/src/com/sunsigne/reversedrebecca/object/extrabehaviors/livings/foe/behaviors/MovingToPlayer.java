e48312eec3e44d74a0a8d6bb52504ad20387b7f2
Florian Murcia <florian.murcia@hotmail.fr>
2022-02-06 18:05:12 +0100

    Foe now stop following you after a max range

--- ReversedRebecca/src/com/sunsigne/reversedrebecca/object/extrabehaviors/livings/foe/behaviors/MovingToPlayer.java
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/object/extrabehaviors/livings/foe/behaviors/MovingToPlayer.java
@@ -4,6 +4,7 @@ import com.sunsigne.reversedrebecca.object.extrabehaviors.behaviors.TickBehavior
 import com.sunsigne.reversedrebecca.object.extrabehaviors.livings.foe.Foe;
 import com.sunsigne.reversedrebecca.object.extrabehaviors.livings.player.Player;
 import com.sunsigne.reversedrebecca.pattern.player.PlayerFinder;
+import com.sunsigne.reversedrebecca.system.Size;
 
 public class MovingToPlayer implements TickBehavior {
 
@@ -19,22 +20,37 @@ public class MovingToPlayer implements TickBehavior {
 	public Foe getExtraBehaviorsObject() {
 		return foe;
 	}
-		
+
 	////////// TICK ////////////
 
-	private Player player;
-	
 	@Override
 	public void tick() {
-		if(player == null) initPlayer();
-		else FollowPlayer();
+		Player player = new PlayerFinder().getPlayer();
+
+		if (player == null)
+			return;
+
+		if (canFollowPlayer(player))
+			FollowPlayer(player);
+
+		else
+			foe.setMotionless();
 	}
-	
-	private void initPlayer() {
-		player = new PlayerFinder().getPlayer();
+
+	private boolean canFollowPlayer(Player player) {
+
+		// foe and player are not not on the same layer
+		if (foe.getHandler() != player.getHandler())
+			return false;
+
+		// player is too far
+		if (new PlayerFinder().isPlayerFutherThan(foe, 19 * Size.XS))
+			return false;
+
+		return true;
 	}
-	
-	private void FollowPlayer() {
+
+	private void FollowPlayer(Player player) {
 		float diffX = foe.getX() - player.getX();
 		float diffY = foe.getY() - player.getY();
 		float distance = (float) Math.sqrt(Math.pow(diffX, 2) + Math.pow(diffY, 2));
@@ -42,5 +58,5 @@ public class MovingToPlayer implements TickBehavior {
 		foe.setVelX(foe.speed * Math.round((-1 / distance) * diffX));
 		foe.setVelY(foe.speed * Math.round((-1 / distance) * diffY));
 	}
-	
+
 }
