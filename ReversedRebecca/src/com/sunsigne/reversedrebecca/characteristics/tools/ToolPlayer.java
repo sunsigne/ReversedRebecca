5f443f1e7b51740f3a22e4b406466202da1f7925
Florian Murcia <florian.murcia@hotmail.fr>
2022-02-07 11:49:22 +0100

    creation of a max difficulty for Tools

--- ReversedRebecca/src/com/sunsigne/reversedrebecca/characteristics/tools/ToolPlayer.java
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/characteristics/tools/ToolPlayer.java
@@ -1,12 +1,14 @@
 package com.sunsigne.reversedrebecca.characteristics.tools;
 
 import com.sunsigne.reversedrebecca.object.characteristics.Difficulty;
+import com.sunsigne.reversedrebecca.pattern.DifficultyComparator;
 import com.sunsigne.reversedrebecca.ressources.FileTask;
 
 public abstract class ToolPlayer implements Difficulty {
 
 	public ToolPlayer() {
 		ToolPlayerList.getList().addObject(this);
+		loadMaxDifficulty();
 		loadDifficulty();
 	}
 
@@ -18,6 +20,23 @@ public abstract class ToolPlayer implements Difficulty {
 
 	////////// DIFFICULTY ////////////
 
+	///// max /////
+
+	private LVL max_difficulty = LVL.NULL;
+
+	public LVL getMaxDifficulty() {
+		return ToolPlayerList.getList().getObject(this).max_difficulty;
+	}
+
+	private void loadMaxDifficulty() {
+		if (getMaxDifficulty() == LVL.NULL) {
+			String txtDifficulty = new FileTask().read(1, "userdata/characteristics/" + getName() + ".csv");
+			ToolPlayerList.getList().getObject(this).max_difficulty = LVL.valueOf(txtDifficulty);
+		}
+	}
+
+	///// current /////
+
 	private LVL difficulty = LVL.NULL;
 
 	@Override
@@ -27,12 +46,15 @@ public abstract class ToolPlayer implements Difficulty {
 
 	@Override
 	public void setDifficulty(LVL difficulty) {
-		ToolPlayerList.getList().getObject(this).difficulty = difficulty;
+		if (new DifficultyComparator().isForbiddenUpgrade(getMaxDifficulty(), difficulty))
+			ToolPlayerList.getList().getObject(this).difficulty = getMaxDifficulty();
+		else
+			ToolPlayerList.getList().getObject(this).difficulty = difficulty;
 	}
 
 	private void loadDifficulty() {
 		if (getDifficulty() == LVL.NULL) {
-			String txtDifficulty = new FileTask().read("userdata/characteristics/" + getName() + ".csv");
+			String txtDifficulty = new FileTask().read(2, "userdata/characteristics/" + getName() + ".csv");
 			setDifficulty(LVL.valueOf(txtDifficulty));
 		}
 	}
--- ReversedRebecca/src/com/sunsigne/reversedrebecca/pattern/DifficultyComparator.java
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/pattern/DifficultyComparator.java
@@ -29,9 +29,36 @@ public class DifficultyComparator {
 		}
 		return false;
 	}
-	
+
 	public boolean isPositiveUpgade(LVL tool_player_lvl, LVL tool_lvl) {
 		return new DifficultyComparator().canUseTool(tool_player_lvl, tool_lvl);
 	}
-	
+
+	public boolean isForbiddenUpgrade(LVL max_lvl, LVL lvl) {
+
+		switch (lvl) {
+
+		case NULL:
+			if (max_lvl == LVL.NULL)
+				return false;
+		case CYAN:
+			if (max_lvl == LVL.CYAN)
+				return false;
+		case GREEN:
+			if (max_lvl == LVL.GREEN)
+				return false;
+		case YELLOW:
+			if (max_lvl == LVL.YELLOW)
+				return false;
+		case ORANGE:
+			if (max_lvl == LVL.ORANGE)
+				return false;
+		case RED:
+			if (max_lvl == LVL.RED)
+				return false;
+
+		}
+		return true;
+	}
+
 }
