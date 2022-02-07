0248ad7a180e0505c7f3b4b8937204e3ba08701c
Florian Murcia <florian.murcia@hotmail.fr>
2022-02-07 15:28:07 +0100

    CanInteract now doesn't implements TickBehavior any more

--- ReversedRebecca/src/com/sunsigne/reversedrebecca/object/extrabehaviors/livings/player/behaviors/CanInteract.java
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/object/extrabehaviors/livings/player/behaviors/CanInteract.java
@@ -1,28 +1,21 @@
 package com.sunsigne.reversedrebecca.object.extrabehaviors.livings.player.behaviors;
 
-import com.sunsigne.reversedrebecca.object.extrabehaviors.behaviors.TickBehavior;
+import com.sunsigne.reversedrebecca.object.extrabehaviors.behaviors.Behavior;
 import com.sunsigne.reversedrebecca.object.extrabehaviors.livings.player.Player;
 
-public class CanInteract implements TickBehavior {
+public class CanInteract implements Behavior {
 
 	public CanInteract(Player player) {
 		this.player = player;
 	}
 
 	////////// BEHAVIOR ////////////
-	
+
 	private Player player;
-	
+
 	@Override
 	public Player getExtraBehaviorsObject() {
 		return player;
 	}
-	
-	////////// TICK ////////////
 
-	@Override
-	public void tick() {
-
-	}
-	
 }
