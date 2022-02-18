9219d10fb48462bb7ad5b4e79f6c68bd58a7ec13
Florian Murcia <florian.murcia@hotmail.fr>
2022-02-18 23:18:05 +0100

    suppression of PreventAccidentalTeleportation

--- ReversedRebecca/src/com/sunsigne/reversedrebecca/object/extrabehaviors/livings/LivingObject.java
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/object/extrabehaviors/livings/LivingObject.java
@@ -1,16 +1,14 @@
 package com.sunsigne.reversedrebecca.object.extrabehaviors.livings;
 
 import com.sunsigne.reversedrebecca.object.characteristics.CollisionDetector;
-import com.sunsigne.reversedrebecca.object.characteristics.PathSearcher;
 import com.sunsigne.reversedrebecca.object.extrabehaviors.ExtraBehaviorsObject;
 import com.sunsigne.reversedrebecca.object.extrabehaviors.behaviors.Behavior;
 import com.sunsigne.reversedrebecca.object.extrabehaviors.livings.behaviors.MoveWhenPushed;
 import com.sunsigne.reversedrebecca.object.extrabehaviors.livings.behaviors.MovingToGoal;
-import com.sunsigne.reversedrebecca.object.extrabehaviors.livings.behaviors.PreventingAccidentalTeleportation;
 import com.sunsigne.reversedrebecca.object.extrabehaviors.livings.behaviors.WalkingRender;
 import com.sunsigne.reversedrebecca.object.extrabehaviors.livings.behaviors.WatchingDirection;
 
-public abstract class LivingObject extends ExtraBehaviorsObject implements PathSearcher, CollisionDetector {
+public abstract class LivingObject extends ExtraBehaviorsObject implements CollisionDetector {
 
 	public LivingObject(String name, int x, int y, int speed) {
 		super(x, y);
@@ -29,41 +27,14 @@ public abstract class LivingObject extends ExtraBehaviorsObject implements PathS
 		return name;
 	}
 
-	////////// POSITION ////////////
-
-	// WARNING ! this method won't allow to move the living futher than 64 pixel
-	// away (by tick) from its current pos
-	@Override
-	public void setX(int x) {
-		super.setX(x);
-	}
-
-	// WARNING ! this method won't allow to move the living futher than 64 pixel
-	// away (by tick) from its current pos
-	@Override
-	public void setY(int y) {
-		super.setY(y);
-	}
-
-	public void teleportTo(int x, int y) {
-		removeBehavior(preventingAccidentalTeleportation);
-		setX(x);
-		setY(y);
-		addBehavior(preventingAccidentalTeleportation);
-	}
-
 	////////// BEHAVIOR ////////////
 
-	public Behavior preventingAccidentalTeleportation;
 	public Behavior watchingDirection;
 	public Behavior walkingRender;
 	public Behavior movingToGoal;
 	public Behavior moveWhenPushed;
 
 	private void addLivingBehaviors() {
-
-		preventingAccidentalTeleportation = new PreventingAccidentalTeleportation(this);
-		addBehavior(preventingAccidentalTeleportation);
 
 		watchingDirection = new WatchingDirection(this);
 		addBehavior(watchingDirection);
