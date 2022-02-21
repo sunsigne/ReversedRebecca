ceb54e4c7e526731585dfd5faa9744f8b0867311
Florian Murcia <florian.murcia@hotmail.fr>
2022-02-21 15:48:09 +0100

    slight change of the loading screen (the font)

--- ReversedRebecca/src/com/sunsigne/reversedrebecca/menu/LoadingScreen.java
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/menu/LoadingScreen.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 
+import com.sunsigne.reversedrebecca.ressources.font.FontTask;
 import com.sunsigne.reversedrebecca.ressources.lang.Translatable;
 import com.sunsigne.reversedrebecca.system.Window;
 import com.sunsigne.reversedrebecca.system.mainloop.Updatable;
@@ -25,16 +26,17 @@ public class LoadingScreen implements Updatable {
 
 	////////// RENDER ////////////
 
+	private Font font = new FontTask().createNewFont("dogicabold", 50f);
+	
 	@Override
 	public void render(Graphics g) {
 		g.setColor(Color.BLACK);
 		g.fillRect(0, 0, Window.WIDHT, Window.HEIGHT);
 		
 		String text = new Translatable().getTranslatedText("Loading", file);
-		Font font = new Font("arial", 1, 70);
 		g.setFont(font);
 		g.setColor(Color.WHITE);
-		g.drawString(text, 20, Window.HEIGHT - 40);		
+		g.drawString(text.toUpperCase() + "...", 30, Window.HEIGHT - 40);		
 	}
 
 }
