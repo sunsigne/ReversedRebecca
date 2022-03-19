a02d305c7f6addc3c305a1a7f3cad5aad642b102
Florian Murcia <florian.murcia@hotmail.fr>
2022-03-19 08:20:52 +0400

    creation of the ConditionAnalyser for Instructions

---
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/instructions/ConditionAnalyser.java
@@ -0,0 +1,35 @@
+package com.sunsigne.reversedrebecca.instructions;
+
+public class ConditionAnalyser {
+
+	public ConditionAnalyser(String target) {
+		valueToCheck = String.valueOf(target.split("\\?")[0]);
+
+		trueAction = String.valueOf(String.valueOf(target.split("\\?")[1])).split(":")[0];
+		falseAction = String.valueOf(String.valueOf(target.split("\\?")[1])).split(":")[1];
+	}
+
+	////////// VALUE ////////////
+
+	private String valueToCheck;
+
+	public String getValueToCheck() {
+		return valueToCheck;
+	}
+
+	////////// OUTPUT ////////////
+
+	private boolean isMet;
+
+	public void setMet(boolean isMet) {
+		this.isMet = isMet;
+	}
+
+	private String trueAction;
+	private String falseAction;
+
+	public String getAction() {
+		return isMet ? trueAction : falseAction;
+	}
+
+}
--- ReversedRebecca/src/com/sunsigne/reversedrebecca/instructions/instruction/SightInstruction.java
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/instructions/instruction/SightInstruction.java
@@ -1,5 +1,6 @@
 package com.sunsigne.reversedrebecca.instructions.instruction;
 
+import com.sunsigne.reversedrebecca.instructions.ConditionAnalyser;
 import com.sunsigne.reversedrebecca.instructions.InstructionList;
 import com.sunsigne.reversedrebecca.object.extrabehaviors.livings.LivingObject;
 import com.sunsigne.reversedrebecca.pattern.player.PlayerFinder;
@@ -29,32 +30,27 @@ public class SightInstruction implements Instruction {
 	@Override
 	public void doAction(LivingObject living, String target) {
 
-		String goal = String.valueOf(target.split(",")[0]);
-		boolean isGoalInSight = false;
-
-		String inSighAction = String.valueOf(target.split(",")[1]);
-		String unseenAction = String.valueOf(target.split(",")[2]);
-
+		ConditionAnalyser condition = new ConditionAnalyser(target);
+	
 		for (Updatable tempUpdatable : living.getHandler().getList()) {
 			if (tempUpdatable instanceof LivingObject == false)
 				continue;
 
 			LivingObject tempLiving = (LivingObject) tempUpdatable;
 
-			if (goal.equalsIgnoreCase("PLAYER")) {
-				isGoalInSight = new SightFinder(living, new PlayerFinder().getPlayer()).isGoalInSight();
+			if (condition.getValueToCheck().equalsIgnoreCase("PLAYER")) {
+				condition.setMet(new SightFinder(living, new PlayerFinder().getPlayer()).isGoalInSight());
 				break;
 			}
 
-			if (tempLiving.getName().equalsIgnoreCase(target)) {
-				isGoalInSight = new SightFinder(living, tempLiving).isGoalInSight();
+			if (tempLiving.getName().equalsIgnoreCase(condition.getValueToCheck())) {
+				condition.setMet(new SightFinder(living, tempLiving).isGoalInSight());
 				break;
 			}
 		}
 
-		String sighAction = isGoalInSight ? inSighAction : unseenAction;
 		Instruction instruction = InstructionList.getList().getObject(new GotoInstruction());
-		instruction.doAction(living, sighAction);
+		instruction.doAction(living, condition.getAction());
 	}
 
 }
