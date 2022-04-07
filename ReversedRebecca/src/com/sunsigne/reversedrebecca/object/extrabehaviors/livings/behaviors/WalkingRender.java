994fbc3be885a2df8edbffc862388b62bb5ad8d4
Florian Murcia <florian.murcia@hotmail.fr>
2022-04-07 11:56:16 +0400

    creation of the HD debug mode

--- ReversedRebecca/src/com/sunsigne/reversedrebecca/object/extrabehaviors/livings/behaviors/WalkingRender.java
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/object/extrabehaviors/livings/behaviors/WalkingRender.java
@@ -9,6 +9,8 @@ import com.sunsigne.reversedrebecca.object.extrabehaviors.behaviors.TickBehavior
 import com.sunsigne.reversedrebecca.object.extrabehaviors.livings.LivingObject;
 import com.sunsigne.reversedrebecca.pattern.Cycloid;
 import com.sunsigne.reversedrebecca.ressources.images.ImageTask;
+import com.sunsigne.reversedrebecca.ressources.layers.LAYER;
+import com.sunsigne.reversedrebecca.system.mainloop.Updatable;
 
 public class WalkingRender implements TickBehavior, RenderBehavior {
 
@@ -58,6 +60,27 @@ public class WalkingRender implements TickBehavior, RenderBehavior {
 
 	////////// TEXTURE ////////////
 
+	private static boolean hd;
+
+	public static void setHD(boolean hd) {
+		WalkingRender.hd = hd;
+
+		for (LAYER tempLayer : LAYER.values()) {
+
+			if (tempLayer.getName().contains("content") == false)
+				continue;
+
+			for (Updatable tempUpdatable : tempLayer.getHandler().getList()) {
+
+				if (tempUpdatable instanceof LivingObject == false)
+					continue;
+
+				LivingObject tempLiving = (LivingObject) tempUpdatable;
+				((WalkingRender) tempLiving.walkingRender).loadAnimations();
+			}
+		}
+	}
+
 	@SuppressWarnings("unchecked")
 	private Cycloid<BufferedImage>[] walking = new Cycloid[4];
 	private BufferedImage[] standing = new BufferedImage[4];
@@ -68,15 +91,15 @@ public class WalkingRender implements TickBehavior, RenderBehavior {
 		walking[DIRECTION.RIGHT.getNum()] = new Cycloid<BufferedImage>(loadAnimation(DIRECTION.RIGHT));
 		walking[DIRECTION.UP.getNum()] = new Cycloid<BufferedImage>(loadAnimation(DIRECTION.UP));
 		walking[DIRECTION.DOWN.getNum()] = new Cycloid<BufferedImage>(loadAnimation(DIRECTION.DOWN));
-		
+
 		standing[DIRECTION.LEFT.getNum()] = loadImage("standing_" + DIRECTION.LEFT.getName());
 		standing[DIRECTION.RIGHT.getNum()] = loadImage("standing_" + DIRECTION.RIGHT.getName());
 		standing[DIRECTION.UP.getNum()] = loadImage("standing_" + DIRECTION.UP.getName());
 		standing[DIRECTION.DOWN.getNum()] = loadImage("standing_" + DIRECTION.DOWN.getName());
-		
+
 		ground = loadImage("ground");
 	}
-	
+
 	private BufferedImage[] loadAnimation(DIRECTION direction) {
 
 		BufferedImage img0 = loadImage("walking_" + direction.getName() + "_0");
@@ -84,10 +107,11 @@ public class WalkingRender implements TickBehavior, RenderBehavior {
 
 		return new BufferedImage[] { img0, img1 };
 	}
-	
+
 	private BufferedImage loadImage(String imageName) {
-		String imagePath = "textures/characters/" + living.getName() + "/" + imageName;
-		String backupImagePath = "textures/characters/" + "error" + "/" + imageName;
+		String hd = WalkingRender.hd ? "hd/" : "";
+		String imagePath = "textures/characters/" + living.getName() + "/" + hd + imageName;
+		String backupImagePath = "textures/characters/" + "error" + "/" + hd + imageName;
 
 		return new ImageTask().loadImage(imagePath, backupImagePath, false);
 	}
@@ -98,7 +122,7 @@ public class WalkingRender implements TickBehavior, RenderBehavior {
 	public void render(Graphics g) {
 		g.drawImage(getImage(), living.getX(), living.getY(), living.getWidth(), living.getHeight(), null);
 	}
-	
+
 	private BufferedImage getImage() {
 		int facing = living.getFacing().getNum();
 
