f72d136d243d3cd3bb47c86b31a4d1e744572f92
Florian Murcia <florian.murcia@hotmail.fr>
2022-04-11 22:32:21 +0400

    rollback : bigger time in DualChecker doesn't prevent false positive

--- ReversedRebecca/src/com/sunsigne/reversedrebecca/system/DualChecker.java
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/system/DualChecker.java
@@ -37,7 +37,7 @@ public class DualChecker implements Updatable {
 
 		// wait more than one tick
 		try {
-			Thread.sleep(30);
+			Thread.sleep(20);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
