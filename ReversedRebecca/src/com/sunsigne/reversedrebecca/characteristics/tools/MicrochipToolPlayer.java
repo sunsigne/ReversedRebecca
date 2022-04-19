29233264399eb1664ec3363aca40d6fdd9aecd8d
Florian Murcia <florian.murcia@hotmail.fr>
2022-04-19 21:08:31 +0400

    creation of Microchip tool

---
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/characteristics/tools/MicrochipToolPlayer.java
@@ -0,0 +1,16 @@
+package com.sunsigne.reversedrebecca.characteristics.tools;
+
+public class MicrochipToolPlayer extends ToolPlayer {
+
+	public MicrochipToolPlayer() {
+		
+	}
+	
+	////////// NAME ////////////
+
+	@Override
+	public String getName() {
+		return "microchip";
+	}
+
+}
