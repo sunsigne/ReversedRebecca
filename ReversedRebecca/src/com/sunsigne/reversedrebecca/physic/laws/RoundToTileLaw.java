1efbb7439dafbe8015b6635bf1368c40ab217f2a
Florian Murcia <florian.murcia@hotmail.fr>
2022-04-22 16:55:41 +0400

    RoundToTileLaw now depends on getSize() of the object

--- ReversedRebecca/src/com/sunsigne/reversedrebecca/physic/laws/RoundToTileLaw.java
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/physic/laws/RoundToTileLaw.java
@@ -5,7 +5,6 @@ import java.awt.Graphics;
 import com.sunsigne.reversedrebecca.object.piranha.characteristics.SpeedVariator;
 import com.sunsigne.reversedrebecca.object.piranha.living.player.PiranhaPlayer;
 import com.sunsigne.reversedrebecca.pattern.TilePos;
-import com.sunsigne.reversedrebecca.system.Size;
 import com.sunsigne.reversedrebecca.system.mainloop.Updatable;
 
 public class RoundToTileLaw implements PhysicLaw {
@@ -26,16 +25,16 @@ public class RoundToTileLaw implements PhysicLaw {
 		if (variator instanceof PiranhaPlayer)
 			return;
 
-		int miniX = variator.getX() % Size.M;
-		int miniY = variator.getY() % Size.M;
+		int miniX = variator.getX() % variator.getSize();
+		int miniY = variator.getY() % variator.getSize();
 
 		// if miniX is not possible to reach with known speed
 		// OR if object is not moving in this axe
 		if (miniX % variator.getSpeed() != 0 || variator.isMotionlessbyX())
-			variator.setX(new TilePos().getTilePos(variator.getX(), Size.M));
+			variator.setX(new TilePos().getTilePos(variator.getX(), variator.getSize()));
 
 		if (miniY % variator.getSpeed() != 0 || variator.isMotionlessbyY())
-			variator.setY(new TilePos().getTilePos(variator.getY(), Size.M));
+			variator.setY(new TilePos().getTilePos(variator.getY(), variator.getSize()));
 	}
 
 	////////// RENDER ////////////
