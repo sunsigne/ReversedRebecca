2dc5e6bc08c1cdc63b8e9c0da629fc034bc2374f
Florian Murcia <florian.murcia@hotmail.fr>
2022-04-29 00:14:00 +0400

    a little improvement in LanguageScreen (still unfinished)

--- ReversedRebecca/src/com/sunsigne/reversedrebecca/menu/LanguageScreen.java
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/menu/LanguageScreen.java
@@ -1,7 +1,9 @@
 package com.sunsigne.reversedrebecca.menu;
 
 import java.awt.Color;
+import java.awt.GradientPaint;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -21,7 +23,7 @@ import com.sunsigne.reversedrebecca.system.mainloop.Updatable;
 public class LanguageScreen implements Updatable {
 
 	private String file = "menu.csv";
-	
+
 	public LanguageScreen() {
 		LAYER.MENU.addObject(this);
 
@@ -33,23 +35,44 @@ public class LanguageScreen implements Updatable {
 
 	///// language /////
 
-	private void createLangButtons() {
-
+	private ArrayList<String> getLangList() {
 		File file = new File("ressources/texts");
-		var lang_list = new ArrayList<String>(Arrays.asList(file.list()));
+		var file_list = new ArrayList<String>(Arrays.asList(file.list()));
 
+		ArrayList<String> lang_list = new ArrayList<String>();
+		file_list.forEach(tempfile -> {
+			if (tempfile.contains(".ini") == false)
+				lang_list.add(tempfile);
+		});
+		return lang_list;
+	}
+	
+	int height = 0;
+	
+	private void createLangButtons() {
+		ArrayList<String> lang_list = getLangList();
+		int size = lang_list.size();
 		int x = 0;
 
-		for (String tempLang : lang_list) {
+		for (int index = 0; index < size; index++) {
+
+			if (index != 0) {
+				if (index % 2 == 0) {
+					x = 0;
+					height += 140;
+				} else
+					x = 600;
+			}
+
+			String tempLang = lang_list.get(index);
+
 			GenericListener onPress = () -> chooseLanguage(tempLang);
 			GenericListener onRelease = null;
 
 			String text = new FileTask().read("/texts/" + tempLang + "/lang/name" + ".txt");
 
-			ButtonObject languageButton = new TitleScreenButton(text, 410 + x, 300, 500, 90, onPress, onRelease);
+			ButtonObject languageButton = new TitleScreenButton(text, 410 + x, 480 + height, 500, 90, onPress, onRelease);
 			LAYER.MENU.addObject(languageButton);
-
-			x += 600;
 		}
 	}
 
@@ -69,8 +92,8 @@ public class LanguageScreen implements Updatable {
 		int widht = 420;
 		int height = 90;
 
-		ButtonObject backButton = new TitleScreenButton(text, Window.WIDHT - widht - 20, Window.HEIGHT - height - 10, widht,
-				height, onPress, onRelease);
+		ButtonObject backButton = new TitleScreenButton(text, Window.WIDHT - widht - 20, Window.HEIGHT - height - 10,
+				widht, height, onPress, onRelease);
 		backButton.setFacing(DIRECTION.RIGHT);
 		LAYER.MENU.addObject(backButton);
 	}
@@ -93,10 +116,21 @@ public class LanguageScreen implements Updatable {
 	public void render(Graphics g) {
 		new TransluantLayer().drawGray(g, Window.WIDHT, Window.HEIGHT);
 
-		g.setColor(Color.BLUE);
-		g.fillRect(380, 280, 1200, 150);
-		g.setColor(Color.YELLOW);
-		g.drawRect(380, 280, 1200, 150);
+		Color light_yellow = new Color(255, 204, 0);
+		Color dark_yellow = new Color(255, 163, 0, 80);
+		int[] rect = new int[] { 380, 460 - height / 2, 1200, 150 + height / 2 };
+
+		g.setColor(dark_yellow);
+		g.fillRect(rect[0] - 7, rect[1] - 7, rect[2] + 2 * 7, rect[3] + 2 * 7);
+
+		g.setColor(light_yellow);
+		g.fillRect(rect[0] - 5, rect[1] - 5, rect[2] + 2 * 5, rect[3] + 2 * 5);
+
+		Graphics2D g2d = (Graphics2D) g;
+		GradientPaint paint = new GradientPaint(0, rect[1], new Color(220, 220, 255), 0, rect[1] + rect[3],
+				new Color(0, 0, 200));
+		g2d.setPaint(paint);
+		g2d.fillRect(rect[0], rect[1], rect[2], rect[3]);
 	}
 
 }
