8ca4203eab80e990fc2096306f12480eb2fae6f2
Florian Murcia <florian.murcia@hotmail.fr>
2022-05-08 20:24:55 +0400

    display an error message when tool does not exist in characteristics

--- ReversedRebecca/src/com/sunsigne/reversedrebecca/characteristics/tools/ToolPlayer.java
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/characteristics/tools/ToolPlayer.java
@@ -1,11 +1,16 @@
 package com.sunsigne.reversedrebecca.characteristics.tools;
 
+import javax.swing.JOptionPane;
+
 import com.sunsigne.reversedrebecca.object.characteristics.Difficulty;
 import com.sunsigne.reversedrebecca.object.gui.GUI;
 import com.sunsigne.reversedrebecca.object.gui.GUIList;
 import com.sunsigne.reversedrebecca.object.gui.GUITools;
 import com.sunsigne.reversedrebecca.pattern.DifficultyComparator;
 import com.sunsigne.reversedrebecca.ressources.FileTask;
+import com.sunsigne.reversedrebecca.ressources.sound.SoundTask;
+import com.sunsigne.reversedrebecca.ressources.sound.SoundTask.SOUNDTYPE;
+import com.sunsigne.reversedrebecca.system.Conductor;
 
 public abstract class ToolPlayer implements Difficulty {
 
@@ -39,9 +44,19 @@ public abstract class ToolPlayer implements Difficulty {
 	private void loadMaxDifficulty() {
 		if (getMaxDifficulty() == LVL.NULL) {
 			String txtDifficulty = new FileTask().read(getName() + "MaxLvl", file);
+			if (txtDifficulty.isEmpty())
+				stopApp();
+
 			ToolPlayerList.getList().getObject(this).max_difficulty = LVL.valueOf(txtDifficulty);
 			updateGUITools();
 		}
+	}
+
+	private void stopApp() {
+		new SoundTask().play(SOUNDTYPE.ERROR, "sound/error");
+		JOptionPane.showMessageDialog(null,
+				"An existing or required tool in this level is undefined in ressources/userdata/characteristics.csv");
+		new Conductor().stopApp();
 	}
 
 	///// current /////
