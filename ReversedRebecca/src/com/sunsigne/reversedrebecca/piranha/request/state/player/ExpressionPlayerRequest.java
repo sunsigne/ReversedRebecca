5ad4657c4b4818c634833dda10ebe996f4b22e95
Florian Murcia <florian.murcia@hotmail.fr>
2022-05-17 10:49:10 +0400

    Piranha files are now fully comatible with new Action system

---
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/piranha/request/state/player/ExpressionPlayerRequest.java
@@ -0,0 +1,36 @@
+package com.sunsigne.reversedrebecca.piranha.request.state.player;
+
+import com.sunsigne.reversedrebecca.object.piranha.PiranhaObject;
+import com.sunsigne.reversedrebecca.object.piranha.living.player.Player;
+import com.sunsigne.reversedrebecca.pattern.player.PlayerFinder;
+import com.sunsigne.reversedrebecca.piranha.request.Request;
+import com.sunsigne.reversedrebecca.piranha.request.RequestList;
+import com.sunsigne.reversedrebecca.piranha.request.ressources.ExpressionRequest;
+
+public class ExpressionPlayerRequest extends ExpressionRequest {
+
+	////////// REQUEST ////////////
+
+	public ExpressionPlayerRequest() {
+		RequestList.getList().addObject(this);
+	}
+
+	private static Request request = new ExpressionPlayerRequest();
+
+	@Override
+	public Request getRequest() {
+		return request;
+	}
+
+	@Override
+	public String getType() {
+		return "EXPRESSION_PLAYER";
+	}
+
+	@Override
+	public void doAction(PiranhaObject object, String target) {
+		Player player = new PlayerFinder().getPlayer();
+		super.doAction(player, target);
+	}
+
+}
