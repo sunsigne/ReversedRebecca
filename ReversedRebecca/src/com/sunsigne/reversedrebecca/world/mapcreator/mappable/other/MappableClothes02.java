d15b1485a50998a2862d354fcb9b5fb6cbd64541
Florian Murcia <florian.murcia@hotmail.fr>
2022-05-18 10:41:41 +0400

    dress up mecanic in lvl000 is now completed

--- ReversedRebecca/src/com/sunsigne/reversedrebecca/world/mapcreator/mappable/other/MappableClothes02.java
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/world/mapcreator/mappable/other/MappableClothes02.java
@@ -5,13 +5,13 @@ import com.sunsigne.reversedrebecca.object.other.DecorationObject;
 import com.sunsigne.reversedrebecca.world.mapcreator.MapCreator;
 import com.sunsigne.reversedrebecca.world.mapcreator.mappable.Mappable;
 
-public class MappableClothes00 implements Mappable {
+public class MappableClothes02 implements Mappable {
 
-	private MappableClothes00() {
+	private MappableClothes02() {
 		new MapCreator().getList().addObject(this);
 	}
 
-	private static Mappable mappable = new MappableClothes00();
+	private static Mappable mappable = new MappableClothes02();
 
 	@Override
 	public Mappable getMappable() {
@@ -22,12 +22,12 @@ public class MappableClothes00 implements Mappable {
 
 	@Override
 	public GameObject createObject(int x, int y) {
-		return new DecorationObject(x, y, "clothes_00");
+		return new DecorationObject(x, y, "clothes_02");
 	}
 
 	@Override
 	public int[] rgbCode() {
-		int[] rgb = { 253, 0, 255 };
+		int[] rgb = { 253, 0, 253 };
 		return rgb;
 	}
 
