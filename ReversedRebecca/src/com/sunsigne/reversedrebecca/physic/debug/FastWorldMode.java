1261fef20d43003a6352d5c214252c5ec0e1f2d8
Florian Murcia <florian.murcia@hotmail.fr>
2022-05-18 17:54:17 +0400

    creation of FastWorldMode (debug)

---
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/physic/debug/FastWorldMode.java
@@ -0,0 +1,52 @@
+package com.sunsigne.reversedrebecca.physic.debug;
+
+import java.awt.Graphics;
+
+import com.sunsigne.reversedrebecca.object.puzzler.door.NullDoorObject;
+import com.sunsigne.reversedrebecca.system.mainloop.Updatable;
+
+public class FastWorldMode extends DebugMode {
+
+	////////// DEBUG MODE ////////////
+
+	private static DebugMode debugMode = new FastWorldMode();
+
+	@Override
+	public DebugMode getDebugMode() {
+		return debugMode;
+	}
+
+	////////// NAME ////////////
+
+	@Override
+	public String getName() {
+		return "debugmode_fast_world";
+	}
+
+	////////// TICK ////////////
+
+	@Override
+	public void tick(Updatable object) {
+		if (getState() == false)
+			return;
+
+		if (object instanceof NullDoorObject)
+			return;
+
+		object.tick();
+		object.tick();
+	}
+
+	////////// RENDER ////////////
+
+	@Override
+	public void beforeObjectRender(Graphics g, Updatable object) {
+
+	}
+
+	@Override
+	public void afterObjectRender(Graphics g, Updatable object) {
+
+	}
+
+}
