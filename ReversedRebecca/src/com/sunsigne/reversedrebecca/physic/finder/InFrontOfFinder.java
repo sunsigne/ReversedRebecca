ee3837716c5be49db2c618ef9b16ff18e6258c96
Florian Murcia <florian.murcia@hotmail.fr>
2022-06-02 15:09:49 +0400

    creation of InFrontOfFinder

---
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/physic/finder/InFrontOfFinder.java
@@ -0,0 +1,40 @@
+package com.sunsigne.reversedrebecca.physic.finder;
+
+import com.sunsigne.reversedrebecca.object.GameObject;
+import com.sunsigne.reversedrebecca.object.characteristics.Facing;
+
+public class InFrontOfFinder {
+
+	public int[] getPos(GameObject object) {
+		if (object instanceof Facing == false)
+			return null;
+
+		int[] pos = new int[2];
+		Facing facing = (Facing) object;
+
+		switch (facing.getFacing()) {
+
+		case LEFT:
+			pos[0] = object.getX() - object.getSize();
+			pos[1] = object.getY();
+			break;
+		case RIGHT:
+			pos[0] = object.getX() + object.getSize();
+			pos[1] = object.getY();
+			break;
+		case UP:
+			pos[0] = object.getX();
+			pos[1] = object.getY() - object.getSize();
+			break;
+		case DOWN:
+			pos[0] = object.getX();
+			pos[1] = object.getY() + object.getSize();
+			break;
+		case NULL:
+			return null;
+		}
+
+		return pos;
+	}
+	
+}
