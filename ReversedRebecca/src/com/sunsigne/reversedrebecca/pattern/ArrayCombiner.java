89aa1630828bc41b24b3390f1e379f72ca176ee4
Florian Murcia <florian.murcia@hotmail.fr>
2022-06-08 16:47:43 +0400

    creation of the pattern ArrayCombiner<T>

---
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/pattern/ArrayCombiner.java
@@ -0,0 +1,26 @@
+package com.sunsigne.reversedrebecca.pattern;
+
+import java.lang.reflect.Array;
+
+public class ArrayCombiner<T> {
+
+	@SuppressWarnings("unchecked")
+	public T[] combine(Class<T> clazz, T[] array, T... elements) {
+
+		T[] combinedArray = (T[]) Array.newInstance(clazz, array.length + elements.length);
+		int index = 0;
+
+		for (T tempElement : array) {
+			combinedArray[index] = tempElement;
+			index++;
+		}
+
+		for (T tempElement : elements) {
+			combinedArray[index] = tempElement;
+			index++;
+		}
+
+		return combinedArray;
+	}
+
+}
