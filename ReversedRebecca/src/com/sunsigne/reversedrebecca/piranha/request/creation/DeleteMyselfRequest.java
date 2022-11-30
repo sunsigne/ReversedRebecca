832c19ee5b9c9e439393a14066a0715406af5716
Florian Murcia <florian.murcia@hotmail.fr>
2022-11-30 15:30:22 +0400

    a.2.5.4 creation of the DeleteMyselfRequest

---
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/piranha/request/creation/DeleteMyselfRequest.java
@@ -0,0 +1,39 @@
+package com.sunsigne.reversedrebecca.piranha.request.creation;
+
+import com.sunsigne.reversedrebecca.object.piranha.PiranhaObject;
+import com.sunsigne.reversedrebecca.piranha.request.Request;
+import com.sunsigne.reversedrebecca.piranha.request.RequestList;
+import com.sunsigne.reversedrebecca.system.mainloop.Handler;
+
+public class DeleteMyselfRequest implements Request {
+
+	////////// REQUEST ////////////
+
+	public DeleteMyselfRequest() {
+		RequestList.getList().addObject(this);
+	}
+
+	private static Request request = new DeleteMyselfRequest();
+
+	@Override
+	public Request getRequest() {
+		return request;
+	}
+
+	@Override
+	public String getType() {
+		return "DELETE_MYSELF";
+	}
+
+	@Override
+	public boolean hasCompactWriting() {
+		return true;
+	}
+
+	@Override
+	public void doAction(PiranhaObject object, String target) {
+		Handler handler = object.getHandler();
+		handler.removeObject(object);
+	}
+
+}
