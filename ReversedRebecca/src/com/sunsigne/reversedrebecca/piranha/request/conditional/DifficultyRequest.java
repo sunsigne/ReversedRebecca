9f6ae07500e5256c01a5ba89ba51f2e8cd423c8d
Florian Murcia <florian.murcia@hotmail.fr>
2023-01-08 10:28:37 +0400

    a.2.13.6 creation of the DifficultyRequest

---
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/piranha/request/conditional/DifficultyRequest.java
@@ -0,0 +1,49 @@
+package com.sunsigne.reversedrebecca.piranha.request.conditional;
+
+import com.sunsigne.reversedrebecca.object.piranha.PiranhaObject;
+import com.sunsigne.reversedrebecca.piranha.request.Request;
+import com.sunsigne.reversedrebecca.piranha.request.RequestList;
+import com.sunsigne.reversedrebecca.system.DifficultyOption;
+
+public class DifficultyRequest extends ConditionalRequest {
+
+	////////// REQUEST ////////////
+
+	public DifficultyRequest() {
+		RequestList.getList().addObject(this);
+	}
+
+	private static Request request = new DifficultyRequest();
+
+	@Override
+	public Request getRequest() {
+		return request;
+	}
+
+	@Override
+	public String getType() {
+		return "DIFFICULTY";
+	}
+
+	@Override
+	public boolean hasCompactWriting() {
+		return false;
+	}
+
+	@Override
+	public void doClassicAction(PiranhaObject object, String target) {
+
+	}
+
+	@Override
+	protected String getConditionToCheck(PiranhaObject object) {
+		return null;
+	}
+
+	@Override
+	protected boolean analyseCondition(PiranhaObject object, String target) {
+		String valueToCheck = target.split("\\?")[0];
+		return DifficultyOption.getDifficulty().getName().equalsIgnoreCase(valueToCheck);
+	}
+
+}
