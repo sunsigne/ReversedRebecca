b41f7a259b26b847124002357a422708530f2e91
Florian Murcia <florian.murcia@hotmail.fr>
2023-01-21 16:32:10 +0400

    a.2.17.8 NullHoleObject are now correclty display behind the Player

--- ReversedRebecca/src/com/sunsigne/reversedrebecca/object/puzzler/hole/NullHoleObject.java
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/object/puzzler/hole/NullHoleObject.java
@@ -1,18 +1,74 @@
 package com.sunsigne.reversedrebecca.object.puzzler.hole;
 
-public class NullHoleObject extends HoleObject {
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import com.sunsigne.reversedrebecca.object.characteristics.GroundLayerSendable;
+import com.sunsigne.reversedrebecca.object.characteristics.interactive.TripleAction;
+import com.sunsigne.reversedrebecca.system.mainloop.Updatable;
+
+public class NullHoleObject extends HoleObject implements GroundLayerSendable {
 
 	public NullHoleObject(DIRECTION facing, int x, int y) {
 		super(LVL.NULL, facing, x, y);
 	}
 
-	////////// INTERACTION ////////////
-
-	@Override
-	protected void loadTripleAction() {
-//		String noActionText = new Translatable().getTranslatedText("DoorLocked", getFile());
-//		Action unlockAction = new UnlockAction(this);
-//		tripleAction = new TripleAction(noActionText, unlockAction, null, null);
+	private NullHoleObject(int x, int y) {
+		super(LVL.NULL, DIRECTION.NULL, x, y);
+		this.onGround = true;
 	}
 
+	////////// INTERACTION ////////////
+
+	private TripleAction tripleAction;
+
+	@Override
+	public TripleAction getTripleAction() {
+		return tripleAction;
+	}
+
+	protected void loadTripleAction() {
+		//String noActionText = new Translatable().getTranslatedText("HoleLoose", FilePath.ACTION);
+		//Action digAction = new DigAction(this);
+		//tripleAction = new TripleAction(noActionText, digAction, null, null);
+	}
+
+	////////// GROUND LAYER SENDABLE ////////////
+
+	@Override
+	public Updatable getReplacementUpdatable() {
+		return new NullHoleObject(getX(), getY());
+	}
+
+	////////// TICK ////////////
+
+	private boolean onGround;
+
+	@Override
+	public void tick() {
+		if (onGround)
+			return;
+
+		onGround = true;
+		sendToGround();
+	}
+
+	////////// TEXTURE ////////////
+
+	public BufferedImage getImage() {
+		if (getFacing() == DIRECTION.NULL)
+			return null;
+
+		return super.getImage();
+	}
+
+	////////// RENDER ////////////
+
+	@Override
+	public void render(Graphics g) {
+		if (getFacing() == DIRECTION.NULL)
+			return;
+
+		super.render(g);
+	}
 }
