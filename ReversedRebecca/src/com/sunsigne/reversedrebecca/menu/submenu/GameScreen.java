3d956990574a3c3e28738f9cc7569482017dc371
Florian Murcia <florian.murcia@hotmail.fr>
2023-01-30 15:12:00 +0400

    a.2.22.1 GameScreen now displays a warning message

--- ReversedRebecca/src/com/sunsigne/reversedrebecca/menu/submenu/GameScreen.java
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/menu/submenu/GameScreen.java
@@ -3,6 +3,7 @@ package com.sunsigne.reversedrebecca.menu.submenu;
 import com.sunsigne.reversedrebecca.menu.MenuScreen;
 import com.sunsigne.reversedrebecca.object.buttons.ButtonObject;
 import com.sunsigne.reversedrebecca.object.buttons.TitleScreenButton;
+import com.sunsigne.reversedrebecca.object.buttons.TitleScreenText;
 import com.sunsigne.reversedrebecca.pattern.listener.GenericListener;
 import com.sunsigne.reversedrebecca.ressources.layers.LAYER;
 
@@ -10,6 +11,8 @@ public class GameScreen extends SubMenuScreen {
 
 	public GameScreen() {
 		super();
+		loadText();
+		
 		createResetButton();
 	}
 
@@ -18,6 +21,27 @@ public class GameScreen extends SubMenuScreen {
 	@Override
 	public String getName() {
 		return "game";
+	}
+
+	////////// TEXT ////////////
+
+	private void loadText() {
+		String text = null;
+		TitleScreenText resetDetail;
+		int x = 325 + 416;
+		int y = 503 + 104;
+
+		// your progress will be ...
+		text = translate("ResetDetail" + "1");
+		resetDetail = new TitleScreenText(text, x, y + 146 - 56);
+		resetDetail.setFontSize(18f);
+		LAYER.MENU.addObject(resetDetail);
+
+		// ... permanently lost
+		text = translate("ResetDetail" + "2");
+		resetDetail = new TitleScreenText(text, x, y + 181 - 56);
+		resetDetail.setFontSize(18f);
+		LAYER.MENU.addObject(resetDetail);
 	}
 
 	////////// SUB MENU ////////////
@@ -31,7 +55,7 @@ public class GameScreen extends SubMenuScreen {
 
 	private void createResetButton() {
 		GenericListener onPress = () -> new ResetScreen();
-		ButtonObject button = new TitleScreenButton(translate("Reset"), 741, 658, 415, 80, onPress, null) {
+		ButtonObject button = new TitleScreenButton(translate("Reset"), 741, 607, 415, 80, onPress, null) {
 			
 			@Override
 			public String getSound() {
