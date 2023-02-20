4340e71706c13620827a874bb9384ba7fb7eeaef
Florian Murcia <florian.murcia@hotmail.fr>
2023-02-20 07:53:30 +0400

    0.3.5.3 creation of the MenuRequest

---
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/piranha/request/conditional/MenuRequest.java
@@ -0,0 +1,68 @@
+package com.sunsigne.reversedrebecca.piranha.request.conditional;
+
+import com.sunsigne.reversedrebecca.object.piranha.PiranhaObject;
+import com.sunsigne.reversedrebecca.physic.PhysicLaw;
+import com.sunsigne.reversedrebecca.physic.PhysicList;
+import com.sunsigne.reversedrebecca.physic.natural.independant.FadeMenuLaw;
+import com.sunsigne.reversedrebecca.piranha.request.Request;
+import com.sunsigne.reversedrebecca.piranha.request.RequestList;
+import com.sunsigne.reversedrebecca.ressources.layers.LAYER;
+
+public class MenuRequest extends ConditionalRequest {
+
+	////////// REQUEST ////////////
+
+	public MenuRequest() {
+		RequestList.getList().addObject(this);
+	}
+
+	private static Request request = new MenuRequest();
+
+	@Override
+	public Request getRequest() {
+		return request;
+	}
+
+	@Override
+	public String getType() {
+		return "MENU";
+	}
+
+	@Override
+	public boolean hasCompactWriting() {
+		return false;
+	}
+
+	@Override
+	public void doClassicAction(PiranhaObject object, String target) {
+
+	}
+
+	@Override
+	protected String getConditionToCheck(PiranhaObject object) {
+		return null;
+	}
+
+	@Override
+	protected boolean analyseCondition(PiranhaObject object, String target) {
+		boolean valueToCheck = Boolean.parseBoolean(String.valueOf(target.split("\\?")[0]));
+		boolean isMenuOpen = isMenuOpen();
+		return valueToCheck == isMenuOpen;
+	}
+
+	private boolean isMenuOpen() {
+		if (LAYER.LOADING.getHandler().getList().isEmpty() == false)
+			return true;
+
+		if (LAYER.MENU.getHandler().getList().isEmpty() == false && isMenuFading() == false)
+			return true;
+
+		return false;
+	}
+
+	private boolean isMenuFading() {
+		PhysicLaw law = PhysicList.getList().getObject(new FadeMenuLaw());
+		return ((FadeMenuLaw) law).isFading();
+	}
+
+}
