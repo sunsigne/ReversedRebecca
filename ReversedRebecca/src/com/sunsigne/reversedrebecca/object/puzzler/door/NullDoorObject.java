394a50dbb3254a1c079879c90984cf6edbcbdad5
Florian Murcia <florian.murcia@hotmail.fr>
2023-05-19 09:16:09 +0400

    0.3.22.3 slight shrink of the hitbox of unlocked doors

--- ReversedRebecca/src/com/sunsigne/reversedrebecca/object/puzzler/door/NullDoorObject.java
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/object/puzzler/door/NullDoorObject.java
@@ -1,11 +1,13 @@
 package com.sunsigne.reversedrebecca.object.puzzler.door;
 
 import java.awt.Graphics;
+import java.awt.Rectangle;
 
 import com.sunsigne.reversedrebecca.object.Wall.COLOR;
 import com.sunsigne.reversedrebecca.object.characteristics.CollisionDetector;
 import com.sunsigne.reversedrebecca.ressources.layers.LAYER;
 import com.sunsigne.reversedrebecca.ressources.sound.SoundTask;
+import com.sunsigne.reversedrebecca.system.Size;
 
 public class NullDoorObject extends DoorObject {
 
@@ -64,4 +66,14 @@ public class NullDoorObject extends DoorObject {
 		});
 	}
 
+	@Override
+	public Rectangle getBounds() {
+		int pixel = 8 * Size.M / 96;
+		int x = getX() + 1 * pixel;
+		int y = getY() + 1 * pixel;
+		int w = getWidth() - 2 * pixel;
+		int h = getHeight() - 2 * pixel;
+		return new Rectangle(x, y, w, h);
+	}
+	
 }
