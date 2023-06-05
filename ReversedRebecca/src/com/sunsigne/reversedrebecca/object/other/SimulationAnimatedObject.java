c1412609e682454ce2e023074c131c750ea88419
Florian Murcia <florian.murcia@hotmail.fr>
2023-06-05 13:39:24 +0400

    0.3.24.6 (WIP) creation of the first simulation (trash can)

--- ReversedRebecca/src/com/sunsigne/reversedrebecca/object/other/SimulationAnimatedObject.java
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/object/other/SimulationAnimatedObject.java
@@ -3,14 +3,14 @@ package com.sunsigne.reversedrebecca.object.other;
 public class SimulationAnimatedObject extends AnimatedDecorationObject {
 
 	public SimulationAnimatedObject(int x, int y, int w, int h, String path, String name) {
-		super(x, y, w, h, name, 5, 3, false);
+		super(x, y, w, h, path + "/" + name, 5, 3, false);
 	}
 
 	////////// NAME ////////////
 
 	@Override
 	protected String getPath() {
-		return "textures/workshop/" + "000" + "/";
+		return "textures/workshop/";
 	}
 
 	@Override
