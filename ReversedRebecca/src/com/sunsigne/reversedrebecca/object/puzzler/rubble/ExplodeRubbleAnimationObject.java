b656577c8f6cc6438f07456ae227360310a47426
Florian Murcia <florian.murcia@hotmail.fr>
2023-06-14 21:35:09 +0400

    0.3.25.12u changeing solving BombPuzzle animation

--- ReversedRebecca/src/com/sunsigne/reversedrebecca/object/puzzler/rubble/ExplodeRubbleAnimationObject.java
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/object/puzzler/rubble/ExplodeRubbleAnimationObject.java
@@ -1,15 +1,10 @@
 package com.sunsigne.reversedrebecca.object.puzzler.rubble;
 
-import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
-import com.sunsigne.reversedrebecca.object.other.BonusText;
 import com.sunsigne.reversedrebecca.object.puzzler.PuzzlerAnimationObject;
 import com.sunsigne.reversedrebecca.pattern.cycloid.LimitedCycloid;
-import com.sunsigne.reversedrebecca.ressources.FilePath;
 import com.sunsigne.reversedrebecca.ressources.images.ImageTask;
-import com.sunsigne.reversedrebecca.ressources.lang.Translatable;
-import com.sunsigne.reversedrebecca.ressources.layers.LAYER;
 import com.sunsigne.reversedrebecca.system.Size;
 
 public class ExplodeRubbleAnimationObject extends PuzzlerAnimationObject {
@@ -32,59 +27,8 @@ public class ExplodeRubbleAnimationObject extends PuzzlerAnimationObject {
 	public void tick() {
 		super.tick();
 
-		final int gap = 8;
-
 		if (time % 4 == 0)
 			animation.cycle();
-
-		switch (time) {
-		case ANIMATION_TIME - 1:
-			displayText();
-			break;
-
-		case ANIMATION_TIME - 20:
-			bonusText.setMotionless();
-			break;
-
-		case 24 + gap:
-		case 16 + gap:
-		case 8 + gap:
-			hiddenText = true;
-			break;
-		case 20 + gap:
-		case 12 + gap:
-		case 4 + gap:
-			hiddenText = false;
-			break;
-		case 1 + gap:
-			LAYER.WORLD_TEXT.getHandler().removeObject(bonusText);
-			break;
-		}
-
-	}
-
-	private BonusText bonusText;
-	private boolean hiddenText;
-
-	private void displayText() {
-		String text = new Translatable().getTranslatedText("BOOM", FilePath.BONUS_TEXT);
-
-		bonusText = new BonusText(text, getX() + getWidth() / 4 + Size.XS / 6, getY() + Size.XS / 3, false) {
-
-			@Override
-			public void tick() {
-
-			}
-
-			@Override
-			public void render(Graphics g) {
-				if (hiddenText == false)
-					super.render(g);
-			}
-		};
-
-		bonusText.setVelY(-2);
-		LAYER.WORLD_TEXT.addObject(bonusText);
 	}
 
 	////////// TEXTURE ////////////
