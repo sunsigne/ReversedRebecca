602ef605e44aa31ea49df07af5606432e7e2a472
Florian Murcia <florian.murcia@hotmail.fr>
2023-06-26 09:58:28 +0400

    0.3.25.40 "report a bug" button now redirect on the website form

--- ReversedRebecca/src/com/sunsigne/reversedrebecca/system/FormTask.java
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/system/FormTask.java
@@ -1,36 +1,29 @@
 package com.sunsigne.reversedrebecca.system;
 
-import java.awt.Toolkit;
-import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.StringSelection;
-
-import javax.swing.JOptionPane;
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class FormTask {
 
 	private final String LINK = "https://form.dragnsurvey.com/survey/r/04b4b3ff";
 
+	////////// USEFUL ////////////
+
+	public String getValidUrl(String text) {
+		return text.replace(" ", "%20").replace("=", "%3D").replace("\"", "%22")
+				.replace(System.getProperty("line.separator"), "%0D%0A");
+	}
+
 	////////// EMAIL ////////////
 
 	public void sendRequest() {
-
-		// copy link to clipboard
-		Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-		StringSelection selection = new StringSelection(LINK);
-		clipboard.setContents(selection, null);
-
-		// writing message
-		String message = "Le lien suivant a été copié :" + System.getProperty("line.separator") + LINK;
-		message = message.concat(System.getProperty("line.separator") + System.getProperty("line.separator"));
-		message = message.concat("Ce lien vous permettra de soummettre un rapport d'erreur.");
-		message = message.concat(System.getProperty("line.separator") + System.getProperty("line.separator"));
-		message = message.concat("Merci également de renseigner dans votre rapport le contenu éventuel");
-		message = message.concat(System.getProperty("line.separator"));
-		message = message.concat("du ou de vos fichers \"crash_report.txt\", puis de les supprimer.");
-
-		// show message to inform
-		new BugFinder().openUserData();
-		JOptionPane.showMessageDialog(null, message);
+		try {
+			Desktop.getDesktop().browse(new URI(LINK));
+		} catch (IOException | URISyntaxException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
