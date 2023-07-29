3562bfdd8cc9972affaa776ccc49b52cfa74868f
Florian Murcia <florian.murcia@hotmail.fr>
2023-07-29 07:10:42 +0400

    0.3.26.77 changeing a door response choice to sarah in lvl001

--- ReversedRebecca/src/com/sunsigne/reversedrebecca/Infos.java
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/Infos.java
@@ -5,7 +5,7 @@ import javax.swing.ImageIcon;
 public class Infos {
 
 	public static final String NAME = "Reversed Rebecca";
-	public static final String VERSION = "v.0.3.26.76";
+	public static final String VERSION = "v.0.3.26.77";
 	public static final boolean IS_DEV_VERSION = System.getProperty("java.class.path").contains("\\git\\");
 	public static final ImageIcon ICON = new ImageIcon("icon.png");
 
