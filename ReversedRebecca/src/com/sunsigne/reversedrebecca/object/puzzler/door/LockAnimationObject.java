9d64bf6365107bcf3fd97c9e4da5055accf0208f
Florian Murcia <florian.murcia@hotmail.fr>
2023-08-11 00:36:54 +0400

    0.3.27.22 improving LockAnimationObject

--- ReversedRebecca/src/com/sunsigne/reversedrebecca/object/puzzler/door/LockAnimationObject.java
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/object/puzzler/door/LockAnimationObject.java
@@ -7,8 +7,8 @@ public class LockAnimationObject extends UnlockAnimationObject {
 
 	public LockAnimationObject(int x, int y) {
 		super(x, y);
-		setY(getY() - Size.XL / 8);
-		setVelY(3);
+		setY(getY() - Size.XS - Size.XL / 8);
+		setVelY(5);
 	}
 
 	////////// NAME ////////////
@@ -24,7 +24,7 @@ public class LockAnimationObject extends UnlockAnimationObject {
 	protected void loadImages() {
 		String path = "textures/puzzler/" + super.getName() + "_";
 
-		lockedImage = new ImageTask().loadImage(path + "1");
+		lockedImage = new ImageTask().loadImage(path + "0");
 		unlockedImage = new ImageTask().loadImage(path + "0");
 
 		currentImage = lockedImage;
