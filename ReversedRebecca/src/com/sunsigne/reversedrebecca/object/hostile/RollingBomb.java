5d58d9a1eb00388e11d0bf75c2e3dff6d79a2917
Florian Murcia <florian.murcia@hotmail.fr>
2023-12-27 05:29:31 +0400

    patch for 0.3.30.13

--- ReversedRebecca/src/com/sunsigne/reversedrebecca/object/hostile/RollingBomb.java
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/object/hostile/RollingBomb.java
@@ -1,23 +1,13 @@
 package com.sunsigne.reversedrebecca.object.hostile;
 
-import java.awt.Graphics;
-import java.awt.image.BufferedImage;
-
-import com.sunsigne.reversedrebecca.object.GameObject;
 import com.sunsigne.reversedrebecca.object.characteristics.CollisionDetector;
 import com.sunsigne.reversedrebecca.object.characteristics.CollisionReactor;
 import com.sunsigne.reversedrebecca.object.piranha.living.player.Player;
-import com.sunsigne.reversedrebecca.object.puzzler.animation.ExplodeRubbleAnimationObject;
-import com.sunsigne.reversedrebecca.pattern.player.PlayerFinder;
-import com.sunsigne.reversedrebecca.ressources.images.ImageTask;
-import com.sunsigne.reversedrebecca.ressources.sound.SoundTask;
-import com.sunsigne.reversedrebecca.ressources.sound.SoundTask.SOUNDTYPE;
-import com.sunsigne.reversedrebecca.system.Size;
 
-public class RollingBomb extends GameObject implements CollisionReactor {
+public class RollingBomb extends ExplodingBomb implements CollisionReactor {
 
 	public RollingBomb(int x, int y) {
-		super(x, y, Size.XL / 2, Size.XL / 2);
+		super(x, y);
 	}
 
 	public RollingBomb(int x, int y, int w, int h) {
@@ -30,73 +20,14 @@ public class RollingBomb extends GameObject implements CollisionReactor {
 		return 2;
 	}
 
-	public void movingtoPlayer() {
-		Player player = new PlayerFinder().getPlayer();
-		if (player == null)
-			return;
-
-		float diffX = getX() - player.getX();
-		float diffY = getY() - player.getY();
-		float distance = (float) Math.sqrt(Math.pow(diffX, 2) + Math.pow(diffY, 2)) / 6;
-
-		setVelX(getSpeed() * Math.round((-1 / distance) * diffX));
-		setVelY(getSpeed() * Math.round((-1 / distance) * diffY));
-	}
-
 	////////// TICK ////////////
 
+	@Override
 	public int getExplodingTime() {
 		return 35;
 	}
 
-	private int time;
-
-	@Override
-	public void tick() {
-		time++;
-
-		if (time >= getExplodingTime())
-			explode();
-	}
-
-	protected void explode() {
-		var handler = getHandler();
-		if (handler == null)
-			return;
-
-		new SoundTask().playSound(SOUNDTYPE.SOUND, "explosion_small");
-		handler.removeObject(this);
-		handler.addObject(new ExplodeRubbleAnimationObject(getX(), getY()));
-	}
-
-	////////// TEXTURE ////////////
-
-	private BufferedImage image;
-
-	public BufferedImage getImage() {
-		if (image == null)
-			image = new ImageTask().loadImage("textures/tools/bomb_null");
-		return image;
-	}
-
-	////////// RENDER ////////////
-
-	@Override
-	public void render(Graphics g) {
-		g.drawImage(getImage(), getX(), getY(), getWidth(), getHeight(), null);
-	}
-
 	////////// COLLISION ////////////
-
-	@Override
-	public boolean isBlockingSight() {
-		return false;
-	}
-
-	@Override
-	public boolean isBlockingPath() {
-		return false;
-	}
 
 	@Override
 	public void collidingReaction(CollisionDetector detectorObject) {
@@ -104,7 +35,7 @@ public class RollingBomb extends GameObject implements CollisionReactor {
 			return;
 
 		collidingReaction(detectorObject, false, () -> {
-			new PlayerFinder().getPlayer().removeHp();
+			removeHp();
 			explode();
 		});
 	}
