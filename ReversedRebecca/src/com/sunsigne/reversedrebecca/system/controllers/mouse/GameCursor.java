70309c40c0bea48e2ec3cd27bf52c7f98e2ec6c2
Florian Murcia <florian.murcia@hotmail.fr>
2024-07-08 14:10:28 +0400

    0.3.30.61 cursor texture on sheet

--- ReversedRebecca/src/com/sunsigne/reversedrebecca/system/controllers/mouse/GameCursor.java
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/system/controllers/mouse/GameCursor.java
@@ -1,19 +1,21 @@
 package com.sunsigne.reversedrebecca.system.controllers.mouse;
 
 import java.awt.Cursor;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 
 import com.sunsigne.reversedrebecca.ressources.images.ImageTask;
+import com.sunsigne.reversedrebecca.ressources.images.SheetableImage;
 import com.sunsigne.reversedrebecca.system.Window;
 import com.sunsigne.reversedrebecca.system.controllers.ControllerManager;
 import com.sunsigne.reversedrebecca.system.mainloop.Game;
 
 public class GameCursor {
 
-	public static final int SPEED = 12;	
+	public static final int SPEED = 12;
 	private static Cursor cursor;
 
 	public void refreshCursor() {
@@ -32,19 +34,65 @@ public class GameCursor {
 
 	////////// CURSOR TYPE ////////////
 
-	public enum CURSOR_TYPE {
-		NULL("null_cursor"), NORMAL("game_cursor");
+	public enum CURSOR_TYPE implements SheetableImage {
+		NULL("null"), NORMAL("game");
 
 		private Cursor cursor;
+		private String name;
 
 		CURSOR_TYPE(String name) {
-			BufferedImage img = new ImageTask().loadImage("textures/cursor/" + name);
-			cursor = Toolkit.getDefaultToolkit().createCustomCursor(rescaleImage(img), new Point(0, 0), name);
+			this.name = name;
 		}
 
 		public Cursor getCursor() {
+			if (cursor == null) {
+				BufferedImage sheet = new ImageTask().loadImage("textures/" + "cursor");
+				BufferedImage image = getSheetSubImage(sheet);
+				cursor = Toolkit.getDefaultToolkit().createCustomCursor(rescaleImage(image), new Point(0, 0), name);
+			}
 			return cursor;
 		}
+
+		////////// UPDATABLE ////////////
+
+		@Override
+		public void tick() {
+
+		}
+
+		@Override
+		public void render(Graphics g) {
+
+		}
+
+		////////// TEXTURE ////////////
+
+		@Override
+		public int getSheetWidth() {
+			return 8;
+		}
+
+		@Override
+		public int getSheetHeight() {
+			return 10;
+		}
+
+		@Override
+		public int getSheetRowCriterion() {
+			return 1;
+		}
+
+		@Override
+		public int getSheetColCriterion() {
+			switch (name) {
+			case "null":
+				return 1;
+			case "game":
+				return 2;
+			}
+			return 0;
+		}
+
 	}
 
 	private static BufferedImage rescaleImage(BufferedImage image) {
