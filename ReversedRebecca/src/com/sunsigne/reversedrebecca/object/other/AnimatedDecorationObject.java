17a348f589aa8f16788c76728332b01708648603
Florian Murcia <florian.murcia@hotmail.fr>
2024-07-10 13:13:47 +0400

    0.3.30.65 other animation texture on sheet

--- ReversedRebecca/src/com/sunsigne/reversedrebecca/object/other/AnimatedDecorationObject.java
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/object/other/AnimatedDecorationObject.java
@@ -2,14 +2,16 @@ package com.sunsigne.reversedrebecca.object.other;
 
 import java.awt.image.BufferedImage;
 
-import com.sunsigne.reversedrebecca.pattern.FormattedString;
+import com.sunsigne.reversedrebecca.pattern.ArrayCombiner;
 import com.sunsigne.reversedrebecca.pattern.cycloid.Cycloid;
 import com.sunsigne.reversedrebecca.pattern.cycloid.LimitedCycloid;
 import com.sunsigne.reversedrebecca.pattern.list.GameList;
 import com.sunsigne.reversedrebecca.pattern.list.LISTTYPE;
+import com.sunsigne.reversedrebecca.ressources.images.Animation;
 import com.sunsigne.reversedrebecca.ressources.images.ImageTask;
+import com.sunsigne.reversedrebecca.ressources.images.SheetableImage;
 
-public class AnimatedDecorationObject extends DecorationObject {
+public class AnimatedDecorationObject extends DecorationObject implements SheetableImage {
 
 	public AnimatedDecorationObject(int x, int y, int w, int h, String name, int animation_time, boolean cycle) {
 		super(x, y, w, h, name);
@@ -52,38 +54,45 @@ public class AnimatedDecorationObject extends DecorationObject {
 
 	protected Cycloid<BufferedImage> animation;
 
+	@Override
+	public int getSheetColCriterion() {
+		return -1;
+	}
+
+	@Override
+	public int getSheetRowCriterion() {
+		return 1;
+	}
+
+	@Override
+	public int getSheetWidth() {
+		return getWidth() / 6;
+	}
+
+	@Override
+	public int getSheetHeight() {
+		return getHeight() / 6;
+	}
+
 	protected int getNumberOfTimesFirstImageIsRepeated() {
 		return 0;
 	}
 
 	private void loadAnimation(boolean cycle) {
+		BufferedImage sheet = new ImageTask().loadImage(getPath() + getName());
+		Animation animationImages = new Animation(sheet, getSheetWidth(), getSheetHeight());
 
-		String path = getPath() + getName() + "_";
-		ImageTask loader = new ImageTask();
-
+		BufferedImage firstImage = getSheetSubImage(sheet, 1);
 		GameList<BufferedImage> list = new GameList<>(LISTTYPE.ARRAY);
-		int index = 0;
-		BufferedImage image = null;
 
-		do {
-			String formated_index = new FormattedString().getNumber(index);
-			image = loader.loadImage(path + formated_index, true);
+		for (int repeat = 0; repeat < getNumberOfTimesFirstImageIsRepeated(); repeat++)
+			list.addObject(new ImageTask().drawCopyOf(firstImage));
 
-			if (image == null)
-				break;
+		BufferedImage[] firstImages = new BufferedImage[list.getList().size()];
+		firstImages = list.getList().toArray(firstImages);
 
-			list.addObject(image);
-
-			if (index == 0)
-				for (int repeat = 0; repeat < getNumberOfTimesFirstImageIsRepeated(); repeat++)
-					list.addObject(loader.drawCopyOf(image));
-
-			index++;
-		} while (true);
-
-		// converting the list into an array
-		BufferedImage[] images = new BufferedImage[list.getList().size()];
-		images = list.getList().toArray(images);
+		BufferedImage images[] = new ArrayCombiner<BufferedImage>().combine(BufferedImage.class, firstImages,
+				animationImages.getImages());
 
 		if (cycle)
 			animation = new Cycloid<BufferedImage>(images);
