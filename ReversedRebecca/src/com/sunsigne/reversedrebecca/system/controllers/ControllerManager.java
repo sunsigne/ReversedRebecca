6605599a598b97ba01fa493186ce94be4664efaf
Florian Murcia <florian.murcia@hotmail.fr>
2024-08-16 16:49:58 +0400

    0.3.31.57 addition of the warning "gamepad support unfinished"

--- ReversedRebecca/src/com/sunsigne/reversedrebecca/system/controllers/ControllerManager.java
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/system/controllers/ControllerManager.java
@@ -1,5 +1,14 @@
 package com.sunsigne.reversedrebecca.system.controllers;
 
+import java.awt.image.BufferedImage;
+
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
+import com.sunsigne.reversedrebecca.ressources.images.ImageTask;
+import com.sunsigne.reversedrebecca.ressources.lang.Language;
+import com.sunsigne.reversedrebecca.ressources.sound.SoundTask;
+import com.sunsigne.reversedrebecca.ressources.sound.SoundTask.SOUNDTYPE;
 import com.sunsigne.reversedrebecca.system.controllers.gamepad.GamepadAdapter;
 import com.sunsigne.reversedrebecca.system.controllers.gamepad.GamepadController;
 import com.sunsigne.reversedrebecca.system.controllers.gamepad.GamepadManager;
@@ -33,9 +42,9 @@ public class ControllerManager {
 	}
 
 	public void setUsingGamepad(boolean usingGamepad) {
-		if(usingGamepad == false)
+		if (usingGamepad == false)
 			GamepadController.pressing.clear();
-		
+
 		if (this.usingGamepad != usingGamepad)
 			updateGamepadDiplay(usingGamepad);
 
@@ -52,8 +61,27 @@ public class ControllerManager {
 	}
 
 	private static void activeGamepadDisplay() {
+		displayGamepadWarning();
 		Game.getInstance().setCursor(CURSOR_TYPE.NULL.getCursor());
 		resetMousePresetings();
+	}
+
+	private static void displayGamepadWarning() {
+		String lang = Language.getInstance().getLang();
+		String title = "WARNING";
+		String message = "Controller support is still under development. It is unstable on this version of the game.";
+		String path = "textures/other/coming_soon";
+
+		if (lang.equalsIgnoreCase("french")) {
+			title = "AVERTISSEMENT";
+			message = "Le support manette est encore en développement. Il est instable sur cette version du jeu.";
+			path = path.concat("_fr");
+		}
+		
+		BufferedImage image = new ImageTask().loadImage(path);
+		ImageIcon icon = new ImageIcon(image);
+		new SoundTask().playSound(SOUNDTYPE.ERROR, "error");
+		JOptionPane.showMessageDialog(null, message, title, JOptionPane.PLAIN_MESSAGE, icon);
 	}
 
 	private static void resetMousePresetings() {
