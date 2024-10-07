9642103f5c636efb65ff979c0c094359eaca1292
Florian Murcia <florian.murcia@hotmail.fr>
2024-10-07 09:35:50 +0400

    0.4.0.6p fix of some minor issues

--- ReversedRebecca/src/com/sunsigne/reversedrebecca/Infos.java
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/Infos.java
@@ -5,7 +5,7 @@ import javax.swing.ImageIcon;
 public class Infos {
 
 	public static final String NAME = "Reversed Rebecca";
-	public static final String VERSION = "(early-demo) v.0.4.0.5p";
+	public static final String VERSION = "(early-demo) v.0.4.0.6p";
 	// public static final boolean IS_DEV_VERSION = System.getProperty("java.class.path").contains("\\git\\");
 	public static boolean IS_DEV_VERSION = false;
 	public static final ImageIcon ICON = new ImageIcon("icon.png");
