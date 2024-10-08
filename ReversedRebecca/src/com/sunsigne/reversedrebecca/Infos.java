5dc6cac89e0ac21f75b426507a16d1f3d5bb168e
Florian Murcia <florian.murcia@hotmail.fr>
2024-10-08 19:02:35 +0400

    0.4.0.4 fix of the function getting username

--- ReversedRebecca/src/com/sunsigne/reversedrebecca/Infos.java
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/Infos.java
@@ -1,13 +1,64 @@
 package com.sunsigne.reversedrebecca;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 import javax.swing.ImageIcon;
+
+import com.sun.jna.platform.win32.Secur32;
+import com.sun.jna.platform.win32.Secur32Util;
+import com.sunsigne.reversedrebecca.ressources.FileTask;
+import com.sunsigne.reversedrebecca.ressources.lang.Language;
 
 public class Infos {
 
+	////////// GAME ////////////
+
 	public static final String NAME = "Reversed Rebecca";
-	public static final String VERSION = "v.0.4.0.3";
-	// public static final boolean IS_DEV_VERSION = System.getProperty("java.class.path").contains("\\git\\");
+	public static final String VERSION = "v.0.4.0.4";
+	// public static final boolean IS_DEV_VERSION =
+	// System.getProperty("java.class.path").contains("\\git\\");
 	public static boolean IS_DEV_VERSION = true;
 	public static final ImageIcon ICON = new ImageIcon("icon.png");
+
+	////////// USEFUL ////////////
+
+	public static final String USERNAME = getUserName();
+
+	private static String getUserName() {
+		// "player"
+		String path = "texts/" + Language.getInstance().getLang() + "/" + "user.txt";
+		String userName = new FileTask().read(false, path);
+
+		try {
+			// JNA attempt to take real name
+			userName = Secur32Util.getUserNameEx(Secur32.EXTENDED_NAME_FORMAT.NameDisplay);
+		} catch (Exception e) {
+			// PowerShell attempt to take real name
+			try {
+				String command = "powershell.exe Get-ItemProperty -Path 'HKLM:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Authentication\\LogonUI'";
+				Process powerShellProcess = Runtime.getRuntime().exec(command);
+				powerShellProcess.getOutputStream().close();
+
+				BufferedReader reader = new BufferedReader(new InputStreamReader(powerShellProcess.getInputStream()));
+				String line;
+
+				while ((line = reader.readLine()) != null) {
+					if (line.contains("LastLoggedOnDisplayName") == false)
+						continue;
+
+					userName = line.split(" : ")[1];
+				}
+			} catch (Exception e1) {
+				// No futher attempt
+				e1.printStackTrace();
+			}
+		}
+
+		if (userName.contains(" "))
+			userName = userName.split(" ")[0];
+
+		return userName;
+	};
 
 }
