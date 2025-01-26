f8219f2c842788bfb485297234e1f2a8b0d71b55
Florian Murcia <florian.murcia@hotmail.fr>
2025-01-26 07:39:17 +0400

    0.4.1.39 fix of the cursor

--- ReversedRebecca/src/com/sunsigne/reversedrebecca/system/controllers/mouse/GameCursor.java
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/system/controllers/mouse/GameCursor.java
@@ -97,10 +97,10 @@ public class GameCursor {
 	}
 
 	private static BufferedImage rescaleImage(BufferedImage image) {
-		int x = (int) (image.getWidth() / Window.SCALE_X);
-		int y = (int) (image.getHeight() / Window.SCALE_Y);
+		int w = (int) (image.getWidth() / Window.SCALE_X);
+		int h = (int) (image.getHeight() / Window.SCALE_Y);
 
-		BufferedImage img = new BufferedImage(x, y, BufferedImage.TYPE_INT_ARGB);
+		BufferedImage img = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
 		Graphics2D g2d = img.createGraphics();
 
 		g2d.drawImage(image, 0, 0, null);
