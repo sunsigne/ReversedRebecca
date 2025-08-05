de242d4a493859906c52e53f3dff2f4facae7ca6
Florian Murcia <florian.murcia@hotmail.fr>
2025-08-05 13:05:42 +0400

    0.4.7.8 creation of the Frame Impacts and CreateFrameImpactRequest

---
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/object/other/FrameImpact.java
@@ -0,0 +1,52 @@
+package com.sunsigne.reversedrebecca.object.other;
+
+import java.awt.AlphaComposite;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import com.sunsigne.reversedrebecca.system.Window;
+
+public class FrameImpact extends DecorationObject {
+
+	public FrameImpact(String name) {
+		super(0, 0, Window.WIDHT, Window.HEIGHT, name);
+	}
+
+	////////// NAME ////////////
+
+	@Override
+	protected String getPath() {
+		return "textures/other/frameimpact/";
+	}
+
+	@Override
+	public String toString() {
+		var clazz = "FRAME IMPACT";
+		return clazz + " : " + getName().toUpperCase();
+	}
+
+	////////// TICK ////////////
+
+	private final int FRAME_TIME = 5;
+	private int time = FRAME_TIME;
+
+	@Override
+	public void tick() {
+		time--;
+		if (time < 0)
+			removeObject();
+	}
+
+	////////// TEXTURE ////////////
+
+	@Override
+	public int getSheetWidth() {
+		return Window.WIDHT;
+	}
+
+	@Override
+	public int getSheetHeight() {
+		return Window.HEIGHT;
+	}
+
+}
---
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/piranha/request/creation/CreateFrameImpactRequest.java
@@ -0,0 +1,48 @@
+package com.sunsigne.reversedrebecca.piranha.request.creation;
+
+import com.sunsigne.reversedrebecca.object.GameObject;
+import com.sunsigne.reversedrebecca.object.other.FrameImpact;
+import com.sunsigne.reversedrebecca.object.piranha.PiranhaObject;
+import com.sunsigne.reversedrebecca.pattern.GameTimer;
+import com.sunsigne.reversedrebecca.pattern.listener.GenericListener;
+import com.sunsigne.reversedrebecca.piranha.request.Request;
+import com.sunsigne.reversedrebecca.piranha.request.RequestList;
+import com.sunsigne.reversedrebecca.ressources.layers.LAYER;
+
+public class CreateFrameImpactRequest implements Request {
+
+	////////// REQUEST ////////////
+
+	public CreateFrameImpactRequest() {
+		new RequestList().addRequest(this, getType());
+	}
+
+	private static Request request = new CreateFrameImpactRequest();
+
+	@Override
+	public Request getRequest() {
+		return request;
+	}
+
+	@Override
+	public String getType() {
+		return "CREATE_FRAME_IMPACT";
+	}
+
+	@Override
+	public boolean hasCompactWriting() {
+		return false;
+	}
+
+	@Override
+	public void doAction(PiranhaObject object, String target) {
+		int time = Integer.valueOf(target.split(",")[0]);
+		String name = String.valueOf(target.split(",")[1]).toLowerCase();
+		
+		GameObject creation = new FrameImpact(name);
+		GenericListener listener = () -> LAYER.PUZZLE.addObject(creation);
+		new GameTimer(time, listener);
+		
+	}
+
+}
