a8f226f3c07f6de0fede6cb22fd66acfb7daa4bd
flori <flori@predator.home>
2026-04-09 21:03:46 +0400

    0.6.2.15 slight retexturing of the number chest

--- ReversedRebecca/src/com/sunsigne/reversedrebecca/object/buttons/ActionOptionPreview.java
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/object/buttons/ActionOptionPreview.java
@@ -8,7 +8,6 @@ import com.sunsigne.reversedrebecca.object.GameObject;
 import com.sunsigne.reversedrebecca.object.characteristics.Difficulty.LVL;
 import com.sunsigne.reversedrebecca.object.characteristics.Facing.DIRECTION;
 import com.sunsigne.reversedrebecca.object.characteristics.interactive.ActionOption;
-import com.sunsigne.reversedrebecca.object.characteristics.interactive.ActionOption.ACTION_DESIGN;
 import com.sunsigne.reversedrebecca.object.characteristics.interactive.ActionOption.ACTION_HIGHLIGHT;
 import com.sunsigne.reversedrebecca.object.puzzler.PuzzlerObject;
 import com.sunsigne.reversedrebecca.object.puzzler.chest.ChestObject;
@@ -19,7 +18,6 @@ import com.sunsigne.reversedrebecca.ressources.font.FontTask;
 import com.sunsigne.reversedrebecca.ressources.font.TextsOption;
 import com.sunsigne.reversedrebecca.ressources.images.ImageTask;
 import com.sunsigne.reversedrebecca.ressources.images.SheetableImage;
-import com.sunsigne.reversedrebecca.ressources.lang.Language;
 import com.sunsigne.reversedrebecca.system.Size;
 import com.sunsigne.reversedrebecca.system.Window;
 import com.sunsigne.reversedrebecca.system.controllers.ControllerManager;
@@ -46,7 +44,7 @@ public class ActionOptionPreview extends GameObject implements SheetableImage {
 	public PhysicLaw[] getPhysicLinker() {
 		return PhysicLinker.MENU;
 	}
-	
+
 	////////// TICK ////////////
 
 	private Font font;
@@ -55,6 +53,8 @@ public class ActionOptionPreview extends GameObject implements SheetableImage {
 	public void tick() {
 		float size = 24f / (float) Math.sqrt(Window.SCALE_X);
 		font = new FontTask().createNewFont("square_sans_serif_7.ttf", size * TextsOption.getSize());
+		if (puzzler != null)
+			puzzler.refresh();
 	}
 
 	////////// TEXTURE ////////////
@@ -71,7 +71,6 @@ public class ActionOptionPreview extends GameObject implements SheetableImage {
 	}
 
 	private BufferedImage rebecca_img;
-	private BufferedImage coming_soon;
 
 	@Override
 	public int getSheetColCriterion() {
@@ -84,19 +83,8 @@ public class ActionOptionPreview extends GameObject implements SheetableImage {
 	}
 
 	private void loadImages() {
-		BufferedImage sheet = null;
-
-		sheet = new ImageTask().loadImage("textures/characters/rebecca/" + "world");
+		BufferedImage sheet = new ImageTask().loadImage("textures/characters/rebecca/" + "world");
 		rebecca_img = getSheetSubImage(sheet, 1, 2, 16, 16);
-
-		String path = "textures/other/coming_soon";
-		String lang = Language.getInstance().getLang();
-
-		if (lang.equalsIgnoreCase("french")) {
-			path = path.concat("_fr");
-		}
-
-		coming_soon = new ImageTask().loadImage(path);
 	}
 
 	////////// RENDER ////////////
@@ -107,18 +95,12 @@ public class ActionOptionPreview extends GameObject implements SheetableImage {
 			return;
 
 		String text = puzzler.getTripleAction().getAction(0).getDisplayedText();
-		
+
 		int gap = 180 + (10 - text.length() * 6) - (int) (TextsOption.getSize() * 90);
-		
+
 		g.drawImage(rebecca_img, getX() - gap, getY(), getWidth(), getHeight(), null);
-
-		if(ActionOption.getDesign() == ACTION_DESIGN.COLOR) {
-			g.drawImage(puzzler.getImage(), getX() + 100 - gap, getY(), getWidth(), getHeight(), null);
-			puzzler.drawHighlight(g, puzzler.getHighlightImage(), 100 - gap, 0, 0, 0);
-		}
-		else
-			g.drawImage(coming_soon, getX() + 100 - gap, getY() + 20, 125, 66, null);
-
+		g.drawImage(puzzler.getImage(), getX() + 100 - gap, getY(), getWidth(), getHeight(), null);
+		puzzler.drawHighlight(g, puzzler.getHighlightImage(), 100 - gap, 0, 0, 0);
 
 		drawTextAction(g, text, gap);
 	}
