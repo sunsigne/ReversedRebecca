42a39edb5c07b04b7fe1941c9816fe9d74c36a22
flori <flori@predator.home>
2026-08-31 09:09:02 +0400

    0.8.12.3 creation of the "alcohol tool"

---
+++ ReversedRebecca/src/com/sunsigne/reversedrebecca/characteristics/tools/AlcoholToolPlayer.java
@@ -0,0 +1,39 @@
+package com.sunsigne.reversedrebecca.characteristics.tools;
+
+public class AlcoholToolPlayer extends ToolPlayer {
+
+	public AlcoholToolPlayer() {
+
+	}
+
+	////////// TOOL ////////////
+
+	private static ToolPlayer instance = new AlcoholToolPlayer();
+
+	@Override
+	protected ToolPlayer getInstance() {
+		return instance;
+	}
+
+	////////// NAME ////////////
+
+	@Override
+	public int getNum() {
+		return 5;
+	}
+
+	@Override
+	public String getName() {
+		return "alcohol";
+	}
+
+	////////// DIFFICULTY ////////////
+
+	///// max /////
+
+	@Override
+	protected String getDefaultMaxDifficulty() {
+		return LVL.YELLOW.getName().toUpperCase();
+	}
+
+}
